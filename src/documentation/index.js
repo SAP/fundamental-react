f65c63a4497d3bb87107e5f37bca7d0d669bc435
Marat <dias.marat@sap.com>
2018-07-18 15:49:10 -0400

    Updates

--- src/documentation/index.js
+++
@@ -1,9 +0,0 @@
-import { DocsTile, DocsText } from '../documentation/DocsTile/DocsTile'
-import { Separator } from '../documentation/Separator/Separator'
-import { Header } from '../documentation/Header/Header'
-import { Description } from '../documentation/Description/Description'
-import { Import } from '../documentation/Import/Import'
-import { Playground } from '../documentation/Playground/Playground'
-import { Properties } from '../documentation/Properties/Properties'
-
-export {DocsTile, DocsText, Separator, Header, Description, Import, Playground, Properties } 
