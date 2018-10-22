b4bf05901d43cf831d53f9e0163c5df1a291dbb9
Chris Manciero <christopher.manciero@sap.com>
2018-10-22 12:55:43 -0400

    Created InlineHelp component

---
+++ src/InlineHelp/InlineHelp.Component.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { InlineHelp } from '../';
+import {
+  DocsTile,
+  DocsText,
+  Separator,
+  Header,
+  Description,
+  Import,
+  Properties
+} from '../';
+
+export const InlineHelpComponent = () => {
+  const defaultHelpPlacement = `<InlineHelp text="Lorem ipsum dolor sit amet, consectetur adipiscing." placement="bottom-right"/>`;
+  const bottomLeftHelpPlacement = `<InlineHelp text="Lorem ipsum dolor sit amet, consectetur adipiscing." placement="bottom-left"/>`;
+  const rightHelpPlacement = `<InlineHelp text="Lorem ipsum dolor sit amet, consectetur adipiscing." placement="right"/>`;
+  const leftHelpPlacement = `<InlineHelp text="Lorem ipsum dolor sit amet, consectetur adipiscing." placement="left"/>`;
+
+  return (
+    <div>
+      <Header>Inline Help</Header>
+      <Description>
+        Inline help is used to display help text in a popover, often inline with
+        headers, body text and form labels.
+      </Description>
+      <Import module="InlineHelp" path="/fundamental-react/src/" />
+
+      <Separator />
+
+      <Properties
+        type="Inputs"
+        properties={[
+          {
+            name: 'text',
+            description:
+              'String - The text to display in the inline help pop-up.'
+          },
+          {
+            name: 'placement',
+            description:
+              'String - Location for where to display the inline help pop-up.'
+          }
+        ]}
+      />
+
+      <Separator />
+
+      <h2>Default Position</h2>
+      <Description>
+        The default positioning of inline help component is bottom right.
+      </Description>
+      <DocsTile>
+        Bottom Right (Default) &nbsp;
+        <InlineHelp
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing."
+          placement="bottom-right"
+        />
+      </DocsTile>
+      <DocsText>{defaultHelpPlacement}</DocsText>
+      <Separator />
+      <h2>Bottom Left Position</h2>
+      <DocsTile>
+        Bottom Left &nbsp;
+        <InlineHelp
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing."
+          placement="bottom-left"
+        />
+      </DocsTile>
+      <DocsText>{bottomLeftHelpPlacement}</DocsText>
+      <Separator />
+      <h2>Right Position</h2>
+      <DocsTile>
+        Right &nbsp;
+        <InlineHelp
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing."
+          placement="right"
+        />
+      </DocsTile>
+      <DocsText>{rightHelpPlacement}</DocsText>
+      <Separator />
+      <h2>Left Position</h2>
+      <DocsTile>
+        Left &nbsp;
+        <InlineHelp
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing."
+          placement="left"
+        />
+      </DocsTile>
+
+      <DocsText>{leftHelpPlacement}</DocsText>
+
+      <Separator />
+    </div>
+  );
+};
