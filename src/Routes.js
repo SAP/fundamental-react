ed31a77981d752752010fe93a0cf4f7cc2a099b9
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-10-22 17:01:32 -0400

    Issue#10: implement menu component

--- src/Routes.js
+++ src/Routes.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
-import { ActionBarComponent } from './ActionBar/ActionBar.Component'
-import { AlertComponent } from './Alert/Alert.Component'
-import { BadgeComponent } from './Badge/Badge.Component'
-import { BreadcrumbComponent } from './Breadcrumb/Breadcrumb.Component'
-import { ButtonComponent } from './Button/Button.Component'
-import { CalendarComponent } from './Calendar/Calendar.Component'
-import { DatePickerComponent } from './DatePicker/DatePicker.Component' 
-import { DropdownComponent } from './Dropdown/Dropdown.Component'
-import { FormsComponent } from './Forms/Forms.Component'
-import { IconComponent } from './Icon/Icon.Component'
-import { IdentifierComponent } from './Identifier/Identifier.Component'
-import { ImageComponent } from './Image/Image.Component'
-import { InputGroupComponent } from './InputGroup/InputGroup.Component'
-import { ListGroupComponent } from './ListGroup/ListGroup.Component'
-import { MegaMenuComponent } from './MegaMenu/MegaMenu.Component'
-import { SideNavigationComponent } from './SideNavigation/SideNavigation.Component'
-import { TableComponent } from './Table/Table.Component'
-import { TabsComponent } from './Tabs/Tabs.Component'
-import { TagComponent } from './Tag/Tag.Component'
-import { TileComponent } from './Tile/Tile.Component'
-import { ToggleComponent } from './Toggle/Toggle.Component'
-import { TreeComponent } from './Tree/Tree.Component'
+import { ActionBarComponent } from './ActionBar/ActionBar.Component';
+import { AlertComponent } from './Alert/Alert.Component';
+import { BadgeComponent } from './Badge/Badge.Component';
+import { BreadcrumbComponent } from './Breadcrumb/Breadcrumb.Component';
+import { ButtonComponent } from './Button/Button.Component';
+import { CalendarComponent } from './Calendar/Calendar.Component';
+import { DatePickerComponent } from './DatePicker/DatePicker.Component';
+import { DropdownComponent } from './Dropdown/Dropdown.Component';
+import { FormsComponent } from './Forms/Forms.Component';
+import { IconComponent } from './Icon/Icon.Component';
+import { IdentifierComponent } from './Identifier/Identifier.Component';
+import { ImageComponent } from './Image/Image.Component';
+import { InputGroupComponent } from './InputGroup/InputGroup.Component';
+import { ListGroupComponent } from './ListGroup/ListGroup.Component';
+import { MegaMenuComponent } from './MegaMenu/MegaMenu.Component';
+import { MenuComponent } from './Menu/Menu.Component';
+import { SideNavigationComponent } from './SideNavigation/SideNavigation.Component';
+import { TableComponent } from './Table/Table.Component';
+import { TabsComponent } from './Tabs/Tabs.Component';
+import { TagComponent } from './Tag/Tag.Component';
+import { TileComponent } from './Tile/Tile.Component';
+import { ToggleComponent } from './Toggle/Toggle.Component';
+import { TreeComponent } from './Tree/Tree.Component';
 
 export default class Routes extends Component {
     constructor(props) {
@@ -34,8 +35,8 @@ export default class Routes extends Component {
                 { url: '/badge', name: 'Badge and Label', component: BadgeComponent },
                 { url: '/breadcrumb', name: 'Breadcrumb', component: BreadcrumbComponent },
                 { url: '/button', name: 'Button', component: ButtonComponent },
-                { url: '/calendar', name: 'Calendar', component: CalendarComponent},
-                { url: '/datepicker', name: 'Date Picker', component: DatePickerComponent},
+                { url: '/calendar', name: 'Calendar', component: CalendarComponent },
+                { url: '/datepicker', name: 'Date Picker', component: DatePickerComponent },
                 { url: '/dropdown', name: 'Dropdown', component: DropdownComponent },
                 { url: '/forms', name: 'Forms', component: FormsComponent },
                 { url: '/icon', name: 'Icon', component: IconComponent },
@@ -44,6 +45,7 @@ export default class Routes extends Component {
                 { url: '/inputGroup', name: 'Input Group', component: InputGroupComponent },
                 { url: '/listGroup', name: 'List Group', component: ListGroupComponent },
                 { url: '/megaMenu', name: 'Mega Menu', component: MegaMenuComponent },
+                { url: '/menu', name: 'Menu', component: MenuComponent },
                 { url: '/sideNavigation', name: 'Side Navigation', component: SideNavigationComponent },
                 { url: '/table', name: 'Table', component: TableComponent },
                 { url: '/tabs', name: 'Tabs', component: TabsComponent },
@@ -52,7 +54,7 @@ export default class Routes extends Component {
                 { url: '/toggle', name: 'Toggle', component: ToggleComponent },
                 { url: '/tree', name: 'Tree', component: TreeComponent }
             ]
-        }
+        };
     }
 
     render() {
@@ -60,28 +62,29 @@ export default class Routes extends Component {
             <BrowserRouter>
                 <div className="container">
                     <div className="sidebar">
-                    <h1 className="logo">FUNDAMENTAL REACT</h1>
+                        <h1 className="logo">FUNDAMENTAL REACT</h1>
                         <ul className="nav">
                             <li className="side-nav__headers">Components</li>
-                            {
-                                this.state.routes.map(route => {
-                                    return (
-                                        <NavLink className="nav-item" to={{ pathname: route.url }} key={route.url} activeClassName="nav-item--active">{route.name}</NavLink>
-                                    )
-                                })
-                            }
+                            {this.state.routes.map(route => {
+                                return (
+                                    <NavLink
+                                        className="nav-item"
+                                        to={{ pathname: route.url }}
+                                        key={route.url}
+                                        activeClassName="nav-item--active"
+                                    >
+                                        {route.name}
+                                    </NavLink>
+                                );
+                            })}
                         </ul>
                     </div>
                     <div className="content">
                         <div className="content-margin">
                             <Switch>
-                                {
-                                    this.state.routes.map(route => {
-                                        return (
-                                            <Route key={route.url} exact path={route.url} component={route.component} />
-                                        )
-                                    })
-                                }
+                                {this.state.routes.map(route => {
+                                    return <Route key={route.url} exact path={route.url} component={route.component} />;
+                                })}
                             </Switch>
                         </div>
                     </div>
