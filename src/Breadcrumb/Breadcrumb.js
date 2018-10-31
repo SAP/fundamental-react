a936af53a5324d56d5cc4777d7d91a175b6480bc
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-10-31 17:08:52 -0400

    Issue#13: adds link and url options for Breadcrumb, Side Navigation and Mega Menu

--- src/Breadcrumb/Breadcrumb.js
+++ src/Breadcrumb/Breadcrumb.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom';
 
-export const Breadcrumb = (props) => {
+export const Breadcrumb = props => {
     const { children } = props;
-    return (
-        <ul className="fd-breadcrumb">
-            {children}
-        </ul>
-    );
-}
+    return <ul className="fd-breadcrumb">{children}</ul>;
+};
 
-export const BreadcrumbItem = (props) => {
-    const { url, name } = props;
+export const BreadcrumbItem = props => {
+    const { url, link, name } = props;
     return (
-        <BrowserRouter>
-            <li className="fd-breadcrumb__item">
-                <Link className="fd-breadcrumb__link" to={{ pathname: url }} >{name}</Link>
-            </li>
-        </BrowserRouter>
+        <React.Fragment>
+            {link ? (
+                <BrowserRouter>
+                    <li className="fd-breadcrumb__item">
+                        <Link className="fd-breadcrumb__link" to={{ pathname: link }}>
+                            {name}
+                        </Link>
+                    </li>
+                </BrowserRouter>
+            ) : null}
+
+            {url ? (
+                <BrowserRouter>
+                    <li className="fd-breadcrumb__item">
+                        <a className="fd-breadcrumb__link" href={url}>
+                            {name}
+                        </a>
+                    </li>
+                </BrowserRouter>
+            ) : null}
+        </React.Fragment>
     );
-}
+};
 
 BreadcrumbItem.propTypes = {
-    url: PropTypes.string.isRequired,
+    url: PropTypes.string,
+    link: PropTypes.string,
     name: PropTypes.string.isRequired
-}
+};
--- src/MegaMenu/MegaMenu.js
+++ src/MegaMenu/MegaMenu.js
@@ -1,135 +1,159 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { BrowserRouter, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 export const MegaMenu = props => {
-  const { id, children } = props;
-  return (
-    <nav className="fd-mega-menu" id={id}>
-      {children}
-    </nav>
-  );
+    const { id, children } = props;
+    return (
+        <nav className="fd-mega-menu" id={id}>
+            {children}
+        </nav>
+    );
 };
 MegaMenu.propTypes = {
-  id: PropTypes.string
+    id: PropTypes.string
 };
 
 export class MegaMenuList extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.handleSelectChild = this.handleSelectChild.bind(this);
+    constructor(props) {
+        super(props);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleSelectChild = this.handleSelectChild.bind(this);
 
-    let initialState = [];
+        let initialState = [];
 
-    props.links.map(link => {
-      if (link.hasChild) {
-        let id = link.id;
-        let obj = {};
+        props.items.map(item => {
+            if (item.hasChild) {
+                let id = item.id;
+                let obj = {};
 
-        obj[id] = false;
-        initialState = obj;
-      }
-    });
+                obj[id] = false;
+                initialState = obj;
+            }
+        });
 
-    this.state = {
-      selectedItem: "item_2",
-      itemStates: initialState
-    };
-  }
+        this.state = {
+            selectedItem: 'item_2',
+            itemStates: initialState
+        };
+    }
 
-  handleSelectChild(e, id) {
-    this.setState({
-      selectedItem: id
-    });
-  }
+    handleSelectChild(e, id) {
+        this.setState({
+            selectedItem: id
+        });
+    }
 
-  handleSelect(e, id) {
-    const { itemStates } = this.state;
-    let iStates = itemStates;
-    iStates[id] = !iStates[id];
-    Object.keys(iStates).map((key, item) => {
-      if (key == id) {
-        iStates[key] = true;
-      } else {
-        iStates[key] = false;
-      }
-    });
-    this.setState({ itemStates: iStates });
-    this.setState({ selectedItem: id });
-  }
+    handleSelect(e, id) {
+        const { itemStates } = this.state;
+        let iStates = itemStates;
+        iStates[id] = !iStates[id];
+        Object.keys(iStates).map((key, item) => {
+            if (key == id) {
+                iStates[key] = true;
+            } else {
+                iStates[key] = false;
+            }
+        });
+        this.setState({ itemStates: iStates });
+        this.setState({ selectedItem: id });
+    }
 
-  render() {
-    const { links } = this.props;
-    return (
-      <BrowserRouter>
-        <ul className="fd-mega-menu__list">
-          {links.map(link => {
-            return (
-              <li className="fd-mega-menu__item" key={link.id}>
-                <Link
-                  className={`fd-mega-menu__link${
-                    this.state.selectedItem === link.id ? " is-selected" : ""
-                  }${link.hasChild ? " has-child" : ""}${
-                    this.state.itemStates[link.id] && link.hasChild
-                      ? " is-expanded"
-                      : ""
-                  }`}
-                  to={{ pathname: link.url }}
-                  key={link.id}
-                  onClick={e => this.handleSelect(e, link.id)}
-                >
-                  {link.name}
-                </Link>
-                {link.hasChild ? (
-                  <ul
-                    className="fd-mega-menu__sublist"
-                    id={link.id}
-                    aria-hidden={!this.state.itemStates[link.id]}
-                    aria-expanded={this.state.itemStates[link.id]}
-                  >
-                    {link.child.map(ch => {
-                      return (
-                        <li className="fd-mega-menu__subitem" key={ch.id}>
-                          <Link
-                            className={`fd-mega-menu__sublink${
-                              this.state.selectedItem === ch.id
-                                ? " is-selected"
-                                : ""
-                            }`}
-                            to={{ pathname: ch.url }}
-                            key={ch.id}
-                            onClick={e => this.handleSelectChild(e, ch.id)}
-                          >
-                            {ch.name}
-                          </Link>
-                        </li>
-                      );
+    render() {
+        const { items } = this.props;
+        return (
+            <BrowserRouter>
+                <ul className="fd-mega-menu__list">
+                    {items.map(item => {
+                        return (
+                            <li className="fd-mega-menu__item" key={item.id}>
+                                {item.link ? (
+                                    <Link
+                                        className={`fd-mega-menu__link${
+                                            this.state.selectedItem === item.id ? ' is-selected' : ''
+                                        }${item.hasChild ? ' has-child' : ''}${
+                                            this.state.itemStates[item.id] && item.hasChild ? ' is-expanded' : ''
+                                        }`}
+                                        to={{ pathname: item.link }}
+                                        key={item.id}
+                                        onClick={e => this.handleSelect(e, item.id)}
+                                    >
+                                        {item.name}
+                                    </Link>
+                                ) : null}
+
+                                {item.url ? (
+                                    <a
+                                        className={`fd-mega-menu__link${
+                                            this.state.selectedItem === item.id ? ' is-selected' : ''
+                                        }${item.hasChild ? ' has-child' : ''}${
+                                            this.state.itemStates[item.id] && item.hasChild ? ' is-expanded' : ''
+                                        }`}
+                                        href={item.url}
+                                        key={item.id}
+                                        onClick={e => this.handleSelect(e, item.id)}
+                                    >
+                                        {item.name}
+                                    </a>
+                                ) : null}
+
+                                {item.hasChild ? (
+                                    <ul
+                                        className="fd-mega-menu__sublist"
+                                        id={item.id}
+                                        aria-hidden={!this.state.itemStates[item.id]}
+                                        aria-expanded={this.state.itemStates[item.id]}
+                                    >
+                                        {item.child.map(ch => {
+                                            return (
+                                                <li className="fd-mega-menu__subitem" key={ch.id}>
+                                                   {ch.link ? ( <Link
+                                                        className={`fd-mega-menu__sublink${
+                                                            this.state.selectedItem === ch.id ? ' is-selected' : ''
+                                                        }`}
+                                                        to={{ pathname: ch.link }}
+                                                        key={ch.id}
+                                                        onClick={e => this.handleSelectChild(e, ch.id)}
+                                                    >
+                                                        {ch.name}
+                                                    </Link>) : null}
+                                                    {ch.url ? ( <a
+                                                        className={`fd-mega-menu__sublink${
+                                                            this.state.selectedItem === ch.id ? ' is-selected' : ''
+                                                        }`}
+                                                        href={ch.url}
+                                                        key={ch.id}
+                                                        onClick={e => this.handleSelectChild(e, ch.id)}
+                                                    >
+                                                        {ch.name}
+                                                    </a>) : null}
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                ) : null}
+                            </li>
+                        );
                     })}
-                  </ul>
-                ) : null}
-              </li>
-            );
-          })}
-        </ul>
-      </BrowserRouter>
-    );
-  }
+                </ul>
+            </BrowserRouter>
+        );
+    }
 }
 MegaMenuList.propTypes = {
-  links: PropTypes.array.isRequired
+    items: PropTypes.array.isRequired
 };
 
 export const MegaMenuGroup = props => {
-  const { title, children } = props;
-  return (
-    <div className="fd-mega-menu__group">
-      <h1 className="fd-mega-menu__title">{title}</h1>
-      {children}
-    </div>
-  );
+    const { title, children } = props;
+    return (
+        <div className="fd-mega-menu__group">
+            <h1 className="fd-mega-menu__title">{title}</h1>
+            {children}
+        </div>
+    );
 };
 
 MegaMenuGroup.propTypes = {
-  title: PropTypes.string
+    title: PropTypes.string
 };
