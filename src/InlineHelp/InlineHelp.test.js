9eeb094eb9c55b0411ad7afa2cb50902cca30b96
Chris Manciero <christopher.manciero@sap.com>
2018-11-11 10:33:28 -0500

    Unit test for InlineHelp component 100% code coverage

---
+++ src/InlineHelp/InlineHelp.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { InlineHelp } from './InlineHelp';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<InlineHelp />', () => {
+  const handleClick = jest.fn();
+  const defaultInlineHelp = (
+    <InlineHelp
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing."
+      placement="bottom-right"
+    />
+  );
+
+  const inlineHelpBottomLeft = (
+    <InlineHelp
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing."
+      placement="bottom-left"
+    />
+  );
+
+  const inlineHelpLeft = (
+    <InlineHelp
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing."
+      placement="left"
+    />
+  );
+
+  const inlineHelpRight = (
+    <InlineHelp
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing."
+      placement="right"
+    />
+  );
+
+  test('create default InlineHelp component', () => {
+    const component = renderer.create(defaultInlineHelp);
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('create Bottom Left InlineHelp component', () => {
+    const component = renderer.create(inlineHelpBottomLeft);
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('create Left InlineHelp component', () => {
+    const component = renderer.create(inlineHelpLeft);
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('create Right InlineHelp component', () => {
+    const component = renderer.create(inlineHelpRight);
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
