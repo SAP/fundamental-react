ca91eb2bbf7bda91cba4e8bb219734f97dd21c07
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-11-13 16:50:13 -0500

    Issue#36: adds implementation for the Combobox component

--- src/ComboboxInput/ComboboxInput.js
+++ src/ComboboxInput/ComboboxInput.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {} from '../';
+import { Popover, Menu, Separator, Header, Description, Import, Properties } from '..';
 
 // ------------------------------------------- Combobox Input ------------------------------------------
 export const ComboboxInput = props => {
-    const { id, children } = props;
+    const { id, placeholder, menu, compact } = props;
     return (
-        <div className="">
-            {children}
+        <div className="fd-combobox-input">
+            <Popover
+                id={id}
+                noArrow
+                control={
+                    <div className="fd-combobox-control">
+                        <div className={`fd-input-group fd-input-group--after${compact ? ' fd-input-group--compact' : ''}`}>
+                            <input type="text" className={`fd-input${compact ? ' fd-input--compact' : ''}`} id="" placeholder={placeholder} />
+                            <span className="fd-input-group__addon fd-input-group__addon--after fd-input-group__addon--button">
+                                <button className=" fd-button--light sap-icon--navigation-down-arrow" />
+                            </span>
+                        </div>
+                    </div>
+                }
+                body={menu}
+            />
         </div>
     );
 };
 
 ComboboxInput.propTypes = {
-    id: PropTypes.string
+    id: PropTypes.string,
+    placeholder: PropTypes.string,
+    menu: PropTypes.object.isRequired,
+    compact: PropTypes.bool
+};
+
+ComboboxInput.defaultTypes = {
+    compact: false,
+    id: '',
+    placeholder: ''
 };
