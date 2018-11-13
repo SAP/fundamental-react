0ee1820da94e89a29790923e5e550e44636328e2
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-11-13 16:50:13 -0500

    Issue#36: adds implementation for the Combobox component

--- src/ComboboxInput/ComboboxInput.Component.js
+++ src/ComboboxInput/ComboboxInput.Component.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import { } from '../';
+import {} from '../';
 import { DocsTile, DocsText, Separator, Header, Description, Import, Properties } from '..';
-import {  } from '..';
+import { Menu, MenuItem, MenuList, ComboboxInput } from '..';
 
 export const ComboboxInputComponent = () => {
-    const comboboxInputCode = ``;
+    const comboboxInputCode = `<ComboboxInput
+    placeholder="Select Fruit"
+    menu={
+        <Menu>
+            <MenuList>
+                <MenuItem url="/">Pear</MenuItem>
+                <MenuItem url="/">Strawberry</MenuItem>
+                <MenuItem url="/">Raspberry</MenuItem>
+                <MenuItem url="/" isLink>
+                    + New Item
+                </MenuItem>
+            </MenuList>
+        </Menu>
+    }
+/>
 
+<ComboboxInput
+    placeholder="Select Fruit"
+    compact
+    menu={
+        <Menu>
+            <MenuList>
+                <MenuItem url="/">Pear</MenuItem>
+                <MenuItem url="/">Strawberry</MenuItem>
+                <MenuItem url="/">Raspberry</MenuItem>
+                <MenuItem url="/" isLink>
+                    + New Item
+                </MenuItem>
+            </MenuList>
+        </Menu>
+    }
+/>`;
 
     return (
         <div>
             <Header>Combobox Input</Header>
-            
-            <Import module="" path="/fundamental-react/src/" />
+
+            <Import module="ComboboxInput, Menu, MenuItem, MenuList" path="/fundamental-react/src/" />
 
             <Separator />
 
             <Properties
                 type="Inputs"
                 properties={[
-                    { name: '...', description: "... - ... ... ..." }
+                    { name: 'menu', description: 'object (required) - An object containing a Menu component. ' },
+                    { name: 'id', description: 'string (optional) - The id of the component.' },
+                    { name: 'placeholder', description: "string (optional) - Input 'placeholder' attribute. " },
+                    { name: 'compact', description: 'bool (optional) - Set to true to enable compact mode.' }
                 ]}
             />
 
@@ -26,12 +59,43 @@ export const ComboboxInputComponent = () => {
 
             <h2>Combobox Input</h2>
             <DocsTile>
-               
+                <ComboboxInput
+                    placeholder="Select Fruit"
+                    menu={
+                        <Menu>
+                            <MenuList>
+                                <MenuItem url="/">Pear</MenuItem>
+                                <MenuItem url="/">Strawberry</MenuItem>
+                                <MenuItem url="/">Raspberry</MenuItem>
+                                <MenuItem url="/" isLink>
+                                    + New Item
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
+                    }
+                />
+
+                <br />
+
+                <ComboboxInput
+                    placeholder="Select Fruit"
+                    compact
+                    menu={
+                        <Menu>
+                            <MenuList>
+                                <MenuItem url="/">Pear</MenuItem>
+                                <MenuItem url="/">Strawberry</MenuItem>
+                                <MenuItem url="/">Raspberry</MenuItem>
+                                <MenuItem url="/" isLink>
+                                    + New Item
+                                </MenuItem>
+                            </MenuList>
+                        </Menu>
+                    }
+                />
             </DocsTile>
             <DocsText>{comboboxInputCode}</DocsText>
             <Separator />
-
-         
         </div>
     );
 };
