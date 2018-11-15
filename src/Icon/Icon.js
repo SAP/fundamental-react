3878a5a726c90444031b58fb3e7338c175c8d4ed
Chris Manciero <christopher.manciero@sap.com>
2018-11-15 16:50:23 -0500

    added more unit test

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Icon = (props) => {
-  const { glyph, size , clickHandler } = props;
+export const Icon = props => {
+  const { glyph, size, clickHandler } = props;
   return (
-    <span className={`${glyph ? 'sap-icon--' + glyph : ''}${size ? ' sap-icon--' + size : ''}`}
-          onClick={clickHandler}>
-    </span>
+    <span
+      className={`${'sap-icon--' + glyph}${size ? ' sap-icon--' + size : ''}`}
+      onClick={clickHandler}
+    />
   );
-}
+};
 
 Icon.defaultProps = {
-    clickHandler: () => {}
-}
+  clickHandler: () => {}
+};
 
 Icon.propTypes = {
   size: PropTypes.string,
   clickHandler: PropTypes.func,
   glyph: PropTypes.string.isRequired
-}
+};
---
+++ src/Image/Image.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Image } from './Image';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Image />', () => {
+  const image = <Image size="s" photo="https://placeimg.com/400/400/nature" />;
+  const circleImage = (
+    <Image type="circle" photo="https://placeimg.com/400/400/nature" />
+  );
+  test('create image', () => {
+    let component = renderer.create(image);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    component = renderer.create(circleImage);
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
