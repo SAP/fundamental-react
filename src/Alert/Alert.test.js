86c9127537dec5de25f9edf9cb46b944fe1920d7
Chris Manciero <christopher.manciero@sap.com>
2018-11-15 13:24:04 -0500

    Adding unit tests and code coverage to components

---
+++ src/Alert/Alert.test.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Alert } from './Alert';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Alert />', () => {
+  const basicAlert = (
+    <Alert dismissable link="#" linkText="link">
+      Default alert with a
+    </Alert>
+  );
+
+  const basicErrorAlert = (
+    <Alert dismissable type="error" linkText="link">
+      Error message with a
+    </Alert>
+  );
+
+  const basicRTLAlert = (
+    <Alert dismissable rtl link="#" linkText="link">
+      Error message with a
+    </Alert>
+  );
+
+  const nonDismissableAlert = (
+    <Alert link="#" linkText="link">
+      Default alert that cannot be dismissed
+    </Alert>
+  );
+
+  const nonDismissableRTLAlert = (
+    <Alert rtl type="warning" linkText="link">
+      Default alert that cannot be dismissed
+    </Alert>
+  );
+
+  test('create basic alert', () => {
+    let component = renderer.create(basicAlert);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    component = renderer.create(basicErrorAlert);
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    component = renderer.create(basicRTLAlert);
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    component = renderer.create(nonDismissableRTLAlert);
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    let wrapper = shallow(basicAlert);
+    expect(wrapper.state(['isActive'])).toBeTruthy();
+    wrapper.find('button.fd-alert__close').simulate('click');
+    expect(wrapper.state(['isActive'])).toBeFalsy();
+
+    wrapper = shallow(basicRTLAlert);
+    expect(wrapper.state(['isActive'])).toBeTruthy();
+    wrapper.find('button.fd-alert__close').simulate('click');
+    expect(wrapper.state(['isActive'])).toBeFalsy();
+  });
+
+  test('create non-dismissable alert', () => {
+    const component = renderer.create(nonDismissableAlert);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // const wrapper = shallow(basicAlert);
+    // expect(wrapper.state(['isActive'])).toBeTruthy();
+
+    // wrapper.find('button.fd-alert__close').simulate('click');
+
+    // expect(wrapper.state(['isActive'])).toBeFalsy();
+  });
+});
--- src/Button/Button.js
+++ src/Button/Button.js
@@ -2,38 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Button = props => {
-    const { option, type, size, glyph, dropdown, navbar, selected, disabled, typeAttr, onclick, children } = props;
-    return (
-        <button
-            className={`${option ? 'fd-button--' + option : ' fd-button'}${type ? ' fd-button--' + type : ''}${dropdown ? ' fd-dropdown__control' : ''}${size ? ' fd-button--' + size : ''}${glyph ? ' sap-icon--' + glyph : ''}${navbar ? ' fd-global-nav__btn' : ''}${selected ? ' is-selected' : ''}${disabled ? ' is-disabled' : ''}`}
-            selected={selected ? selected : false}
-            disabled={disabled ? disabled : false}
-            type={typeAttr}
-            onClick={onclick}
-        >
-            {children}
-        </button>
-    );
+  const {
+    option,
+    type,
+    size,
+    glyph,
+    dropdown,
+    navbar,
+    selected,
+    disabled,
+    typeAttr,
+    onclick,
+    children
+  } = props;
+  return (
+    <button
+      className={`${option ? 'fd-button--' + option : ' fd-button'}${
+        type ? ' fd-button--' + type : ''
+      }${dropdown ? ' fd-dropdown__control' : ''}${
+        size ? ' fd-button--' + size : ''
+      }${glyph ? ' sap-icon--' + glyph : ''}${
+        navbar ? ' fd-global-nav__btn' : ''
+      }${selected ? ' is-selected' : ''}${disabled ? ' is-disabled' : ''}`}
+      selected={selected ? selected : false}
+      disabled={disabled ? disabled : false}
+      type={typeAttr}
+      onClick={onclick}
+    >
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
-    option: PropTypes.string,
-    type: PropTypes.string,
-    size: PropTypes.string,
-    glyph: PropTypes.string,
-    navbar: PropTypes.bool,
-    dropdown: PropTypes.bool,
-    selected: PropTypes.bool,
-    disabled: PropTypes.bool,
-    typeAttr: PropTypes.string,
-    onclick: PropTypes.func
+  option: PropTypes.string,
+  type: PropTypes.string,
+  size: PropTypes.string,
+  glyph: PropTypes.string,
+  navbar: PropTypes.bool,
+  dropdown: PropTypes.bool,
+  selected: PropTypes.bool,
+  disabled: PropTypes.bool,
+  typeAttr: PropTypes.string,
+  onclick: PropTypes.func
 };
 
 export const ButtonGroup = props => {
-    const { children } = props;
-    return (
-        <div className="fd-button-group" role="group" aria-label="Group label">
-            {children}
-        </div>
-    );
+  const { children } = props;
+  return (
+    <div className="fd-button-group" role="group" aria-label="Group label">
+      {children}
+    </div>
+  );
 };
