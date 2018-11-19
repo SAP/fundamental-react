ace5b9f85e7e2046c030209ad8dfe2741a381513
Inna Atanasova <39598672+InnaAtanasova@users.noreply.github.com>
2018-11-19 16:25:41 -0500

    Update unit-tests.sh

--- ci-scripts/unit-tests.sh
+++ ci-scripts/unit-tests.sh
@@ -2,19 +2,19 @@
 set -e
 set -o pipefail
 
-echo "Running unit tests and code coverage for lib"
+echo "-----"
+echo "Running unit tests and code coverage for core"
 exec 5>&1
-output=$(ng test fundamental-react --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+output=$(ng test src --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
 coverage=$(echo $output | grep -i "does not meet global threshold" || true)
 if [[ -n "$coverage" ]]; then
     echo "Error: Tests did not meet coverage expectations"
     exit 1
 fi
 
-echo "-----"
-echo "Running unit tests and code coverage for core"
+echo "Running unit tests and code coverage for lib"
 exec 5>&1
-output=$(ng test core --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
+output=$(ng test src --watch=false --code-coverage --browsers=ChromeHeadless | tee /dev/fd/5)
 coverage=$(echo $output | grep -i "does not meet global threshold" || true)
 if [[ -n "$coverage" ]]; then
     echo "Error: Tests did not meet coverage expectations"
