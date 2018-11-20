b8e1091095cdd0a9499c94eb5c9796555f41490e
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-11-20 15:52:51 -0500

    adds PropTypes for Table, adds tr for the thead

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -2,28 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Table = props => {
-  const { headers, tableData } = props;
-  return (
-    <table className="fd-table">
-      <thead>
-        {headers.map((header, index) => {
-          return <th key={index}>{header}</th>;
-        })}
-      </thead>
-      <tbody>
-        {tableData.map((row, index) => {
-          return (
-            <tr key={index}>
-              {row.rowData.map((rowData, index) => {
-                return <td key={index}>{rowData}</td>;
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+    const { headers, tableData } = props;
+    return (
+        <table className="fd-table">
+            <thead>
+                <tr>
+                    {headers.map((header, index) => {
+                        return <th key={index}>{header}</th>;
+                    })}
+                </tr>
+            </thead>
+            <tbody>
+                {tableData.map((row, index) => {
+                    return (
+                        <tr key={index}>
+                            {row.rowData.map((rowData, index) => {
+                                return <td key={index}>{rowData}</td>;
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 };
 Table.propTypes = {
-  icons: PropTypes.bool
+    headers: PropTypes.array,
+    tableData: PropTypes.arrayOf(
+        PropTypes.shape({
+            rowData: PropTypes.array
+        }).isRequired
+    ).isRequired
 };
