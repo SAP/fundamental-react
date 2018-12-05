890f351c2965fe2dd3ea548145d8b444bd2ebf2a
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-12-05 11:17:49 -0500

    pass all Calendar inputs from DatePicker

--- src/DatePicker/DatePicker.Component.js
+++ src/DatePicker/DatePicker.Component.js
@@ -3,8 +3,12 @@ import { DocsTile, DocsText, Separator, Header, Description, Import, Properties 
 import { DatePicker } from '../';
 
 export const DatePickerComponent = () => {
-    const defaultDatePickerCode = `<DatePicker />
-<DatePicker compact />`;
+    const defaultDatePickerCode = `<DatePicker disableBeforeDate={new Date(2018, 11, 24, 0, 0, 0, 0)} disableWeekends={true} />
+<DatePicker
+    compact
+    disableWeekday={['Monday', 'Tuesday']}
+    blockedDates={[new Date(2018, 11, 1, 0, 0, 0, 0), new Date(2018, 11, 23, 0, 0, 0, 0)]}
+/>`;
     const enableRangeSelectionDatePickerCode = `<DatePicker enableRangeSelection />
 <DatePicker enableRangeSelection compact />`;
 
@@ -28,8 +32,12 @@ export const DatePickerComponent = () => {
 
             <h2>Simple Date Picker</h2>
             <DocsTile centered>
-                <DatePicker />
-                <DatePicker compact />
+                <DatePicker disableBeforeDate={new Date(2018, 11, 24, 0, 0, 0, 0)} disableWeekends={true} />
+                <DatePicker
+                    compact
+                    disableWeekday={['Monday', 'Tuesday']}
+                    blockedDates={[new Date(2018, 11, 1, 0, 0, 0, 0), new Date(2018, 11, 23, 0, 0, 0, 0)]}
+                />
             </DocsTile>
             <DocsText>{defaultDatePickerCode}</DocsText>
             <Separator />
