9a7e7293649d05042136634fccdf1e65a2d4c127
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-12-11 15:58:04 -0500

    adds more implementation

--- src/Shellbar/Shellbar.Component.js
+++ src/Shellbar/Shellbar.Component.js
@@ -1,63 +1,146 @@
 import React from 'react';
-import { DocsTile, DocsText, Separator, Header, Description, Import, Properties } from '..';
+import { DocsTile, DocsText, Separator, Header, Description, Import, Properties, Menu, MenuList, MenuItem } from '..';
 import { Shellbar } from '..';
 var images = require.context('../../assets', true);
 
 export const ShellbarComponent = () => {
-    const shellbarExampleCode = ``;
+    const shellbarExampleCode = `<Shellbar
+    logo={<img src="//unpkg.com/fiori-fundamentals/dist/images/sap-logo.png" alt="SAP" />}
+    productTitle="Corporate Portal"
+    productMenu={productMenu}
+    subtitle="Subtitle"
+    copilot
+    actions={actions}
+    user={user}
+    userMenu={userMenu}
+    productSwitcher={productSwitcherList}
+    productSwitcherCollapsed={productSwitcherCollapsed}
+/>
+
+const productMenu = [
+    { name: 'Application A', callback: () => alert('Application A selected!') },
+    { name: 'Application B', callback: () => alert('Application B selected!') },
+    { name: 'Application C', callback: () => alert('Application C selected!') },
+    { name: 'Application D', callback: () => alert('Application D selected!') }
+];
+
+const actions = [
+    { glyph: 'bell', notificationCount: 21, label: 'Notification', callback: () => alert('Notification selected!')},
+    { glyph: 'post', notificationCount: 4, label: 'Post', callback: () => alert('Post selected!')},
+    { glyph: 'settings', label: 'Settings', notificationCount: 0, callback: () => alert('Settings selected!'), menu: (
+        <Menu>
+            <MenuList>
+                <MenuItem url="/">Option 1</MenuItem>
+                <MenuItem url="/">Option 2</MenuItem>
+                <MenuItem url="/">Option 3</MenuItem>
+            </MenuList>
+        </Menu>
+    )}
+];
+
+const user = {
+    initials: 'JS',
+    userName: 'John Snow'
+};
+
+const userMenu = [
+    { name: 'Settings', glyph: 'action-settings', size: 's', callback: () => alert('Settings selected!') },
+    { name: 'Sign Out', glyph: 'log', size: 's', callback: () => alert('Sign Out selected!') }
+];
+
+const productSwitcher = {
+    label: 'Product Switcher',
+    glyph: 'grid',
+    callback: () => alert('Product Switcher selected!')
+};
+
+const productSwitcherList = [
+    { title: 'Fiori Home', image: images('./01.png'), callback: () => alert('Fiori Home selected!') },
+    { title: 'S/4 HANA Cloud', image: images('./02.png'), callback: () => alert('S/4 HANA Cloud selected!') },
+    { title: 'Analytics Cloud', image: images('./03.png'), callback: () => alert('Analytics Cloud selected!') },
+    { title: 'Ariba', image: images('./04.png'), callback: () => alert('Ariba selected!') },
+    { title: 'SuccessFactors', image: images('./05.png'), callback: () => alert('SuccessFactors selected!') },
+    { title: 'Commerce Cloud', image: images('./06.png'), callback: () => alert('Commerce Cloud selected!') },
+    { title: 'Gigya', image: images('./07.png'), callback: () => alert('Gigya selected!') },
+    { title: 'Callidus Cloud', image: images('./08.png'), callback: () => alert('Callidus Cloud selected!') },
+    { title: 'Fieldglass', image: images('./09.png'), callback: () => alert('Fieldglass selected!') },
+    { title: 'Concur', image: images('./10.png'), callback: () => alert('Concur selected!') },
+    { title: 'Cloud for Customer', image: images('./11.png'), callback: () => alert('Cloud for Customer selected!')},
+    { title: 'Cloud Portal', image: images('./12.png'), callback: () => alert('Cloud Portal selected!') }
+];
+`;
 
     const actions = [
         {
             glyph: 'bell',
-            notificationCount: 23,
+            notificationCount: 21,
             label: 'Notification',
-            callback: () => alert('Notification button pressed')
+            callback: () => alert('Notification selected!')
         },
         {
-            glyph: 'pool',
-            label: 'Pool',
-            callback: () => alert('Pool button pressed')
+            glyph: 'post',
+            notificationCount: 4,
+            label: 'Post',
+            callback: () => alert('Post selected!')
         },
         {
-            glyph: 'flight',
-            notificationCount: 7,
-            label: 'Flight',
-            callback: () => alert('Flight button pressed')
+            glyph: 'settings',
+            label: 'Settings',
+            notificationCount: 0,
+            callback: () => alert('Settings selected!'),
+            menu: (
+                <Menu>
+                    <MenuList>
+                        <MenuItem url="/">Option 1</MenuItem>
+                        <MenuItem url="/">Option 2</MenuItem>
+                        <MenuItem url="/">Option 3</MenuItem>
+                    </MenuList>
+                </Menu>
+            )
         }
     ];
 
     const user = {
-        initials: 'WW'
+        initials: 'JS',
+        userName: 'John Snow'
     };
 
     const userMenu = [
-        { text: 'Settings', callback: () => alert('Settings clicked!') },
-        { text: 'Sign Out', callback: () => alert('Sign Out clicked!') }
+        { name: 'Settings', glyph: 'action-settings', size: 's', callback: () => alert('Settings selected!') },
+        { name: 'Sign Out', glyph: 'log', size: 's', callback: () => alert('Sign Out selected!') }
     ];
 
- 
-
     const productMenu = [
-        { name: 'Application A', callback: () => alert('Application A clicked!') },
-        { name: 'Application B', callback: () => alert('Application B clicked!') },
-        { name: 'Application C', callback: () => alert('Application C clicked!') },
-        { name: 'Application D', callback: () => alert('Application D clicked!') }
+        { name: 'Application A', callback: () => alert('Application A selected!') },
+        { name: 'Application B', callback: () => alert('Application B selected!') },
+        { name: 'Application C', callback: () => alert('Application C selected!') },
+        { name: 'Application D', callback: () => alert('Application D selected!') }
     ];
 
     const productSwitcherList = [
-        { title: 'Fiori Home', image: images('./01.png') },
-        { title: 'S/4 HANA Cloud', image: images('./02.png') },
-        { title: 'Analytics Cloud', image: images('./03.png') },
-        { title: 'Ariba', image: images('./04.png') },
-        { title: 'SuccessFactors', image: images('./05.png') },
-        { title: 'Commerce Cloud', image: images('./06.png') },
-        { title: 'Gigya', image: images('./07.png') },
-        { title: 'Callidus Cloud', image: images('./08.png') },
-        { title: 'Fieldglass', image: images('./09.png') },
-        { title: 'Concur', image: images('./10.png') },
-        { title: 'Cloud for Customer', image: images('./11.png') },
-        { title: 'Cloud Portal', image: images('./12.png') }
+        { title: 'Fiori Home', image: images('./01.png'), callback: () => alert('Fiori Home selected!') },
+        { title: 'S/4 HANA Cloud', image: images('./02.png'), callback: () => alert('S/4 HANA Cloud selected!') },
+        { title: 'Analytics Cloud', image: images('./03.png'), callback: () => alert('Analytics Cloud selected!') },
+        { title: 'Ariba', image: images('./04.png'), callback: () => alert('Ariba selected!') },
+        { title: 'SuccessFactors', image: images('./05.png'), callback: () => alert('SuccessFactors selected!') },
+        { title: 'Commerce Cloud', image: images('./06.png'), callback: () => alert('Commerce Cloud selected!') },
+        { title: 'Gigya', image: images('./07.png'), callback: () => alert('Gigya selected!') },
+        { title: 'Callidus Cloud', image: images('./08.png'), callback: () => alert('Callidus Cloud selected!') },
+        { title: 'Fieldglass', image: images('./09.png'), callback: () => alert('Fieldglass selected!') },
+        { title: 'Concur', image: images('./10.png'), callback: () => alert('Concur selected!') },
+        {
+            title: 'Cloud for Customer',
+            image: images('./11.png'),
+            callback: () => alert('Cloud for Customer selected!')
+        },
+        { title: 'Cloud Portal', image: images('./12.png'), callback: () => alert('Cloud Portal selected!') }
     ];
+
+    const productSwitcher = {
+        label: 'Product Switcher',
+        glyph: 'grid',
+        callback: () => alert('Product Switcher selected!')
+    };
 
     return (
         <div>
@@ -81,21 +164,16 @@ export const ShellbarComponent = () => {
             <Separator />
             <DocsTile>
                 <Shellbar
-                    logo={
-                        <img
-                            src="//unpkg.com/fiori-fundamentals/dist/images/sap-logo.png"
-                            srcSet="//unpkg.com/fiori-fundamentals/dist/images/sap-logo@2x.png 1x, //unpkg.com/fiori-fundamentals/dist/images/sap-logo@3x.png 2x, //unpkg.com/fiori-fundamentals/dist/images/sap-logo@4x.png 3x"
-                            alt="SAP"
-                        />
-                    }
-                    productTitle='Corporate Portal'
+                    logo={<img src="//unpkg.com/fiori-fundamentals/dist/images/sap-logo.png" alt="SAP" />}
+                    productTitle="Corporate Portal"
                     productMenu={productMenu}
-                    subtitle='Subtitle'
+                    subtitle="Subtitle"
                     copilot
                     actions={actions}
                     user={user}
                     userMenu={userMenu}
-                    productSwitcher={productSwitcherList}
+                    productSwitcherList={productSwitcherList}
+                    productSwitcher={productSwitcher}
                 />
             </DocsTile>
             <DocsText>{shellbarExampleCode}</DocsText>
