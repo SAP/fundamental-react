fc096986a3e50de06db7e04d0ec1fcbb38163b4c
Taushanova-Atanasova, Inna <inna.taushanova-atanasova@sap.com>
2018-12-12 17:02:58 -0500

    Issue#100: updrages to v1.4.0 of FioriFundamentals, adds information and success types for Alert component, renames Badge and Status documentation to Status Indicators, adds counter and notifications to Status Indicators

--- src/Badge/Badge.js
+++ src/Badge/Badge.js
@@ -2,49 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Badge = props => {
-  const { type, modifier, children } = props;
-  return (
-    <span
-      className={`fd-badge${type ? ' fd-badge--' + type : ''}${
-        modifier ? ' fd-badge--' + modifier : ''
-      }`}
-    >
-      {children}
-    </span>
-  );
+    const { type, modifier, children } = props;
+    return (
+        <span className={`fd-badge${type ? ' fd-badge--' + type : ''}${modifier ? ' fd-badge--' + modifier : ''}`}>
+            {children}
+        </span>
+    );
 };
 
 Badge.propTypes = {
-  type: PropTypes.oneOf(['', 'success', 'warning' , 'error']),
-  modifier: PropTypes.oneOf(['', 'pill', 'filled'])
+    type: PropTypes.oneOf(['', 'success', 'warning', 'error']),
+    modifier: PropTypes.oneOf(['', 'pill', 'filled'])
 };
 
 export const Label = props => {
-  const { type, children } = props;
-  return (
-    <span className={`fd-label${type ? ' fd-label--' + type : ''}`}>
-      {children}
-    </span>
-  );
+    const { type, children } = props;
+    return <span className={`fd-label${type ? ' fd-label--' + type : ''}`}>{children}</span>;
 };
 
 Label.propTypes = {
-  type: PropTypes.oneOf(['', 'success', 'warning' , 'error'])
+    type: PropTypes.oneOf(['', 'success', 'warning', 'error'])
 };
 
 export const Status = props => {
-  const { type, glyph, children } = props;
-  return (
-    <span
-      className={`fd-status-label${type ? ' fd-status-label--' + type : ''}${
-        glyph ? ' sap-icon--' + glyph : ''
-      }`}
-    >
-      {children}
-    </span>
-  );
+    const { type, glyph, children } = props;
+    return (
+        <span
+            className={`fd-status-label${type ? ' fd-status-label--' + type : ''}${glyph ? ' sap-icon--' + glyph : ''}`}
+        >
+            {children}
+        </span>
+    );
 };
 Status.propTypes = {
-  type: PropTypes.oneOf(['', 'success', 'warning' , 'error', 'available', 'away', 'busy', 'offline']),
-  glyph: PropTypes.string
+    type: PropTypes.oneOf(['', 'success', 'warning', 'error', 'available', 'away', 'busy', 'offline']),
+    glyph: PropTypes.string
+};
+
+export const Counter = props => {
+    const { notification, children } = props;
+    return (
+        <span className={`fd-counter${notification ? ' fd-counter--notification' : ''}`} aria-label="Unread count">
+            {children}
+        </span>
+    );
+};
+
+Counter.propTypes = {
+    notification: PropTypes.bool
 };
