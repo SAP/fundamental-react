16e2a06088596117d0e2cdaaf8802ecf9ca6fe7f
Chris Manciero <christopher.manciero@sap.com>
2018-12-17 20:59:13 -0500

    add typescript code to breadcrumb

--- src/Breadcrumb/Breadcrumb.tsx
+++ src/Breadcrumb/Breadcrumb.tsx
@@ -1,41 +1,79 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { ReactNode } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
+import { ICommonProps } from '../common/common';
 
-export const Breadcrumb = props => {
-    const { children } = props;
-    return <ul className="fd-breadcrumb">{children}</ul>;
-};
+interface IBreadCrumbProps extends ICommonProps {}
 
-export const BreadcrumbItem = props => {
-    const { url, link, name } = props;
-    return (
-        <React.Fragment>
-            {link ? (
-                <BrowserRouter>
-                    <li className="fd-breadcrumb__item">
-                        <Link className="fd-breadcrumb__link" to={{ pathname: link }}>
-                            {name}
-                        </Link>
-                    </li>
-                </BrowserRouter>
-            ) : null}
+// export const Breadcrumb: React.SFC<IProps> = props => {
+//   const { children } = props;
+//   return <ul className="fd-breadcrumb">{children}</ul>;
+// };
 
-            {url ? (
-                <BrowserRouter>
-                    <li className="fd-breadcrumb__item">
-                        <a className="fd-breadcrumb__link" href={url}>
-                            {name}
-                        </a>
-                    </li>
-                </BrowserRouter>
-            ) : null}
-        </React.Fragment>
-    );
-};
+export function Breadcrumb(props: IBreadCrumbProps): JSX.Element {
+  const { id, children } = props;
+  return (
+    <ul id={id} className="fd-breadcrumb">
+      {children}
+    </ul>
+  );
+}
 
-BreadcrumbItem.propTypes = {
-    url: PropTypes.string,
-    link: PropTypes.string,
-    name: PropTypes.string.isRequired
-};
+interface IBreadCrumbItemProps extends ICommonProps {
+  url?: string;
+  link?: string;
+  name: string;
+}
+
+// export const BreadcrumbItem: React.SFC<IBreadCrumbItemProps> = props => {
+//   const { url, link, name } = props;
+//   return (
+//     <React.Fragment>
+//       {link ? (
+//         <BrowserRouter>
+//           <li className="fd-breadcrumb__item">
+//             <Link className="fd-breadcrumb__link" to={{ pathname: link }}>
+//               {name}
+//             </Link>
+//           </li>
+//         </BrowserRouter>
+//       ) : null}
+
+//       {url ? (
+//         <BrowserRouter>
+//           <li className="fd-breadcrumb__item">
+//             <a className="fd-breadcrumb__link" href={url}>
+//               {name}
+//             </a>
+//           </li>
+//         </BrowserRouter>
+//       ) : null}
+//     </React.Fragment>
+//   );
+// };
+
+export function BreadcrumbItem(props: IBreadCrumbItemProps): JSX.Element {
+  const { id, url, link, name } = props;
+  return (
+    <React.Fragment>
+      {link ? (
+        <BrowserRouter>
+          <li id={id} className="fd-breadcrumb__item">
+            <Link className="fd-breadcrumb__link" to={{ pathname: link }}>
+              {name}
+            </Link>
+          </li>
+        </BrowserRouter>
+      ) : null}
+
+      {url ? (
+        <BrowserRouter>
+          <li id={id} className="fd-breadcrumb__item">
+            <a className="fd-breadcrumb__link" href={url}>
+              {name}
+            </a>
+          </li>
+        </BrowserRouter>
+      ) : null}
+    </React.Fragment>
+  );
+}
