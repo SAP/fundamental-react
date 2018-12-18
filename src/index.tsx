26dce85908ea86db44c4cac58a6cff36f3d591b7
Chris Manciero <christopher.manciero@sap.com>
2018-12-18 09:48:07 -0500

    rename component .js file to .tsx, add typescript support

---
+++ src/index.tsx
@@ -0,0 +1,195 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import {
+  ActionBar,
+  ActionBarBack,
+  ActionBarHeader,
+  ActionBarActions
+} from './ActionBar/ActionBar';
+import { Alert } from './Alert/Alert';
+import { Badge, Label, Status, Counter } from './Badge/Badge';
+import { Breadcrumb, BreadcrumbItem } from './Breadcrumb/Breadcrumb';
+import { Button, ButtonGroup } from './Button/Button';
+import { Calendar } from './Calendar/Calendar';
+import { ComboboxInput } from './ComboboxInput/ComboboxInput';
+import { DatePicker } from './DatePicker/DatePicker';
+import { Dropdown } from './Dropdown/Dropdown';
+import {
+  FormSet,
+  FormItem,
+  FormLabel,
+  FormInput,
+  FormRadio,
+  FormTextarea,
+  FormMessage,
+  FormSelect,
+  FormFieldset,
+  FormLegend
+} from './Forms/Forms';
+import { Icon } from './Icon/Icon';
+import { Identifier } from './Identifier/Identifier';
+import { Image } from './Image/Image';
+import { InlineHelp } from './InlineHelp/InlineHelp';
+import { InputGroup, FormGroup } from './InputGroup/InputGroup';
+import {
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemActions,
+  ListGroupItemCheckbox
+} from './ListGroup/ListGroup';
+import { LocalizationEditor } from './LocalizationEditor/LocalizationEditor';
+import { MegaMenu, MegaMenuList, MegaMenuGroup } from './MegaMenu/MegaMenu';
+import { Menu, MenuList, MenuItem, MenuGroup } from './Menu/Menu';
+import { Modal } from './Modal/Modal';
+import {
+  Navbar,
+  NavbarGroup,
+  NavbarActions,
+  NavbarElement
+} from './Navbar/Navbar';
+import { MultiInput } from './MultiInput/MultiInput';
+import { Pagination } from './Pagination/Pagination';
+import {
+  Panel,
+  PanelGrid,
+  PanelBody,
+  PanelHeader,
+  PanelHead,
+  PanelActions,
+  PanelFilters,
+  PanelContent,
+  PanelFooter
+} from './Panel/Panel';
+import { Popover } from './Popover/Popover';
+import { SearchInput } from './SearchInput/SearchInput';
+import {
+  SideNav,
+  SideNavList,
+  SideNavGroup
+} from './SideNavigation/SideNavigation';
+import { Table } from './Table/Table';
+import { Tabs, TabComponent } from './Tabs/Tabs';
+import { Token } from './Token/Token';
+import {
+  Tile,
+  TileContent,
+  TileMedia,
+  TileActions,
+  ProductTile,
+  ProductTileMedia,
+  TileGrid,
+  ProductTileContent
+} from './Tile/Tile';
+import { Toggle } from './Toggle/Toggle';
+import { Tree } from './Tree/Tree';
+import { Time } from './Time/Time';
+import { TimePicker } from './TimePicker/TimePicker';
+import { Shellbar } from './Shellbar/Shellbar';
+import { DocsTile, DocsText } from './documentation/DocsTile/DocsTile';
+import { Separator } from './documentation/Separator/Separator';
+import { Header } from './documentation/Header/Header';
+import { Description } from './documentation/Description/Description';
+import { Import } from './documentation/Import/Import';
+import { Properties } from './documentation/Properties/Properties';
+import { Playground } from './documentation/Playground/Playground';
+
+export {
+  DocsTile,
+  DocsText,
+  Separator,
+  Header,
+  Description,
+  Import,
+  Properties,
+  Playground,
+  ActionBar,
+  ActionBarBack,
+  ActionBarHeader,
+  ActionBarActions,
+  Alert,
+  Badge,
+  ComboboxInput,
+  Label,
+  Status,
+  Counter,
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  ButtonGroup,
+  Calendar,
+  DatePicker,
+  Dropdown,
+  FormSet,
+  FormItem,
+  FormLabel,
+  FormInput,
+  FormRadio,
+  FormTextarea,
+  FormMessage,
+  FormSelect,
+  FormFieldset,
+  FormLegend,
+  Icon,
+  Identifier,
+  Image,
+  InlineHelp,
+  InputGroup,
+  FormGroup,
+  ListGroup,
+  ListGroupItem,
+  ListGroupItemActions,
+  ListGroupItemCheckbox,
+  LocalizationEditor,
+  MegaMenu,
+  MegaMenuList,
+  MegaMenuGroup,
+  Menu,
+  MenuList,
+  MenuItem,
+  MenuGroup,
+  Modal,
+  MultiInput,
+  Navbar,
+  NavbarGroup,
+  NavbarActions,
+  NavbarElement,
+  Popover,
+  Pagination,
+  Panel,
+  PanelGrid,
+  PanelBody,
+  PanelHeader,
+  PanelHead,
+  PanelActions,
+  PanelFilters,
+  PanelContent,
+  PanelFooter,
+  SearchInput,
+  SideNav,
+  SideNavList,
+  SideNavGroup,
+  Table,
+  Tabs,
+  TabComponent,
+  Token,
+  Tile,
+  TileContent,
+  TileMedia,
+  TileActions,
+  Time,
+  TimePicker,
+  ProductTile,
+  ProductTileMedia,
+  TileGrid,
+  ProductTileContent,
+  Toggle,
+  Tree,
+  Shellbar
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
