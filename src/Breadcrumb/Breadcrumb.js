1eed88f06825fd459fe02b137e26479418c51f29
Chris Manciero <christopher.manciero@sap.com>
2018-12-17 20:57:46 -0500

    undelete breadcrumb remove .tsx

---
+++ src/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+export const Breadcrumb = props => {
+    const { children } = props;
+    return <ul className="fd-breadcrumb">{children}</ul>;
+};
+
+export const BreadcrumbItem = props => {
+    const { url, link, name } = props;
+    return (
+        <React.Fragment>
+            {link ? (
+                <BrowserRouter>
+                    <li className="fd-breadcrumb__item">
+                        <Link className="fd-breadcrumb__link" to={{ pathname: link }}>
+                            {name}
+                        </Link>
+                    </li>
+                </BrowserRouter>
+            ) : null}
+
+            {url ? (
+                <BrowserRouter>
+                    <li className="fd-breadcrumb__item">
+                        <a className="fd-breadcrumb__link" href={url}>
+                            {name}
+                        </a>
+                    </li>
+                </BrowserRouter>
+            ) : null}
+        </React.Fragment>
+    );
+};
+
+BreadcrumbItem.propTypes = {
+    url: PropTypes.string,
+    link: PropTypes.string,
+    name: PropTypes.string.isRequired
+};
