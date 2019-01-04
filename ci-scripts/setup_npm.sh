dee41c6d80232b11ff88775d6da152011db6014e
jenna-badanowski <j.badanowski@sap.com>
2019-01-04 10:17:45 -0800

    automate versioning and publish via travis

---
+++ ci-scripts/setup_npm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+echo "//registry.npmjs.org/:username=${NPM_USERNAME}" >> ~/.npmrc
+echo "//registry.npmjs.org/:email=${NPM_EMAIL}" >> ~/.npmrc
+echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${AUTH_TOKEN}" >> ~/.npmrc
