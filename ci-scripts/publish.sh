1391fd84122708e9d70316f2cf1aad826a3374af
Jenna Badanowski <29607818+jbadan@users.noreply.github.com>
2019-01-08 16:33:46 -0800

    fix: removing automation centered on development branch

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -3,20 +3,12 @@ git config --global user.email "fundamental@sap.com"
 git config --global user.name "fundamental-bot"
 
 git checkout master
-npm install
-npm run build
 
 # update the package verion and commit to the git repository
-npm run std-version
+# npm run std-version
 
 # pushes changes to master
-git push --quiet --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
+# git push --quiet --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH" > /dev/null 2>&1;
 
 # commit changes made by standard-version to develop branch
-git checkout develop
-# git merge master
-# git commit -a -m "chore: merge master into develop [ci skip]"
-git push --quiet --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" develop > /dev/null 2>&1;
-# publish master to npm
-git checkout master
-npm publish
+# npm publish
