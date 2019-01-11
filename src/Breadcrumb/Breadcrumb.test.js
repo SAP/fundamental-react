81cd1a2280114d8fe1d20c3783ee46a201b9c440
Greg Smith <greg.smith03@sap.com>
2019-01-11 10:03:09 -0600

    Added prop spreading tests for Breadcrumb and Button

--- src/Breadcrumb/Breadcrumb.test.js
+++ src/Breadcrumb/Breadcrumb.test.js
@@ -1,36 +1,51 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem } from './Breadcrumb';
 
 describe('<Breadcrumb />', () => {
-  const defaultBreadCrumb = (
-      <Breadcrumb>
-          <BreadcrumbItem url='#' name='Link Text' />
-          <BreadcrumbItem url='#' name='Link Text' />
-          <BreadcrumbItem name='Link Text' />
-      </Breadcrumb>
-  );
+    const defaultBreadCrumb = (
+        <Breadcrumb>
+            <BreadcrumbItem url='#' name='Link Text' />
+            <BreadcrumbItem url='#' name='Link Text' />
+            <BreadcrumbItem name='Link Text' />
+        </Breadcrumb>
+    );
 
-  const breadCrumbRouterLink = (
-      <MemoryRouter>
-          <Breadcrumb className='blue'>
-              <BreadcrumbItem className='blue' name='Link Text' />
-              <BreadcrumbItem link='/' name='Link Text' />
-              <BreadcrumbItem link='/' name='Link Text' />
-          </Breadcrumb>
-      </MemoryRouter>
-  );
+    const breadCrumbRouterLink = (
+        <MemoryRouter>
+            <Breadcrumb className='blue'>
+                <BreadcrumbItem className='blue' name='Link Text' />
+                <BreadcrumbItem link='/' name='Link Text' />
+                <BreadcrumbItem link='/' name='Link Text' />
+            </Breadcrumb>
+        </MemoryRouter>
+    );
 
-  test('create default breadcrumbs', () => {
-    const component = renderer.create(defaultBreadCrumb);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+    test('create default breadcrumbs', () => {
+        const component = renderer.create(defaultBreadCrumb);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 
-  test('create breadcrumbs using router links', () => {
-    const component = renderer.create(breadCrumbRouterLink);
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+    test('create breadcrumbs using router links', () => {
+        const component = renderer.create(breadCrumbRouterLink);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    describe('Prop spreading', () => {
+        xtest('should allow props to be spread to the Breadcrumb component', () => {
+            // placeholder for this test description once that functionality is built
+        });
+
+        test('should allow props to be spread to the BreadcrumbItem component', () => {
+            const element = mount(<BreadcrumbItem data-sample='Sample' name='Name' />);
+
+            expect(
+                element.getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
+        });
+    });
 });
