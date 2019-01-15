ab72ff285d81288c79d10bce8a4ebaffab1776b6
Greg Smith <greg.smith03@sap.com>
2019-01-14 20:54:34 -0600

    chore: Prop spreading unit tests (#184)
    * Added configuration for enzyme adapter
    
    * Added prop spreading tests for ActionBar
    
    * Added prop spreading tests for Alert and Badge
    
    * Added prop spreading tests for Breadcrumb and Button
    
    * Added prop spreading tests for Calendar and ComboboxInput
    
    * Added TODO markers on the comments for skipped tests
    
    * Added prop spreading tests for DatePicker and Dropdown
    
    * Added prop spreading tests for Forms and Icon
    
    * Added prop spreading tests for Identifier and Image
    
    * Added prop spreading tests for InlineHelp and InputGroup
    
    * Added prop spreading tests for ListGroup and LocalizationEditor
    
    * Added prop spreading tests for MegaMenu and Menu
    
    * Added prop spreading tests for Modal and MultiInput
    
    * Added prop spreading tests for Pagination and Panel
    
    * Added prop spreading tests for Popover and SearchInput
    
    * Added prop spreading tests for Shellbar and SideNavigation
    
    * Added prop spreading tests for SideNavigation and Table
    
    * Added prop spreading tests for Tabs and Tile
    
    * Added prop spreading tests for Time and TimePicker
    
    * Added prop spreading tests for Toggle and Token
    
    * Added prop spreading tests for Tree

--- src/Toggle/Toggle.test.js
+++ src/Toggle/Toggle.test.js
@@ -1,58 +1,73 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { mount, shallow } from 'enzyme';
 import { Toggle } from './Toggle';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 describe('<Toggle />', () => {
-  const defaultToggle = <Toggle>Normal toggle</Toggle>;
-  const checkedToggle = (
-      <Toggle className='blue' size='s'
-          checked>
-      Small toggle
-      </Toggle>
-  );
-  const disabledToggle = <Toggle disabled>Normal toggle</Toggle>;
-  const disabledCheckedToggle = (
-      <Toggle checked disabled>
-      Normal toggle
-      </Toggle>
-  );
+    const defaultToggle = <Toggle>Normal toggle</Toggle>;
+    const checkedToggle = (
+        <Toggle className='blue' size='s'
+            checked>
+            Small toggle
+        </Toggle>
+    );
+    const disabledToggle = <Toggle disabled>Normal toggle</Toggle>;
+    const disabledCheckedToggle = (
+        <Toggle checked disabled>
+            Normal toggle
+        </Toggle>
+    );
 
-  test('create toggle component', () => {
-    // Default toggle
-    let component = renderer.create(defaultToggle);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    test('create toggle component', () => {
+        // Default toggle
+        let component = renderer.create(defaultToggle);
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
 
-    // checked toggle
-    component = renderer.create(checkedToggle);
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+        // checked toggle
+        component = renderer.create(checkedToggle);
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
 
-    // disabled toggle
-    component = renderer.create(disabledToggle);
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+        // disabled toggle
+        component = renderer.create(disabledToggle);
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
 
-    // checked disabled toggle
-    component = renderer.create(disabledCheckedToggle);
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+        // checked disabled toggle
+        component = renderer.create(disabledCheckedToggle);
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 
-  test('Toggle state change', () => {
-    const wrapper = shallow(defaultToggle);
+    test('Toggle state change', () => {
+        const wrapper = shallow(defaultToggle);
 
-    // check that toggle is not checked
-    expect(wrapper.state(['checked'])).toBeFalsy();
+        // check that toggle is not checked
+        expect(wrapper.state(['checked'])).toBeFalsy();
 
-    // simulate changing toggle
-    wrapper.find('input[type="checkbox"]').simulate('change');
+        // simulate changing toggle
+        wrapper.find('input[type="checkbox"]').simulate('change');
 
-    // check that toggle is checked
-    expect(wrapper.state(['checked'])).toBeTruthy();
-  });
+        // check that toggle is checked
+        expect(wrapper.state(['checked'])).toBeTruthy();
+    });
+
+    describe('Prop spreading', () => {
+        test('should allow props to be spread to the Toggle component', () => {
+            const element = mount(<Toggle data-sample='Sample' />);
+
+            expect(
+                element.getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
+        });
+
+        xtest('should allow props to be spread to the Toggle component\'s label element', () => {
+            // TODO: placeholder for this test description once that functionality is built
+        });
+
+        xtest('should allow props to be spread to the Toggle component\'s input element', () => {
+            // TODO: placeholder for this test description once that functionality is built
+        });
+    });
 });
--- src/Token/Token.test.js
+++ src/Token/Token.test.js
@@ -1,15 +1,26 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Token } from './Token';
 
 describe('<Token />', () => {
-  test('create token component', () => {
-    let component = renderer.create(<Token>Bibendum</Token>);
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    test('create token component', () => {
+        let component = renderer.create(<Token>Bibendum</Token>);
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
 
-    component = renderer.create(<Token className='blue'>Bibendum</Token>);
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+        component = renderer.create(<Token className='blue'>Bibendum</Token>);
+        tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    describe('Prop spreading', () => {
+        test('should allow props to be spread to the Token component', () => {
+            const element = mount(<Token data-sample='Sample' />);
+
+            expect(
+                element.getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
+        });
+    });
 });
