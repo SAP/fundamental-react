dfc5ada473b167d1e80e24dc2dc5681958a60ffc
Greg Smith <greg.smith03@sap.com>
2019-01-17 15:10:33 -0600

    Added props for prop spreading

--- src/InputGroup/InputGroup.js
+++ src/InputGroup/InputGroup.js
@@ -1,9 +1,8 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-export const FormGroup = props => {
-    const { children } = props;
-    return <div className='fd-form__group'>{children}</div>;
+export const FormGroup = ({ children, ...props}) => {
+    return <div {...props} className='fd-form__group'>{children}</div>;
 };
 
 FormGroup.propTypes = {
@@ -61,22 +60,30 @@ export class InputGroup extends Component {
           inputId,
           inputName,
           inputPlaceholder,
+          inputProps,
+          inputValue,
+          numberUpButtonProps,
+          numberDownButtonProps,
+          searchButtonProps,
           addonPos,
           addon,
           glyph,
           actions,
           compact,
-          children
+          children,
+          ...props
       } = this.props;
 
       switch (inputType) {
           case 'number':
               return (
                   <div
+                      {...props}
                       className={`fd-input-group fd-input-group--after${
                           compact ? ' fd-input-group--compact' : ''
                       }`}>
                       <input
+                          {...inputProps}
                           className={`${compact ? 'fd-input fd-input--compact' : ''}`}
                           id={inputId}
                           name={inputName}
@@ -85,10 +92,12 @@ export class InputGroup extends Component {
                           value={this.state.value} />
                       <span className='fd-input-group__addon fd-input-group__addon--button fd-input-group__addon--after'>
                           <button
+                              {...numberUpButtonProps}
                               aria-label='Step up'
                               className='fd-input-group__button fd-input-group__button--step-up sap-icon--slim-arrow-up'
                               onClick={this.handleUp} />
                           <button
+                              {...numberDownButtonProps}
                               aria-label='Step down'
                               className='fd-input-group__button fd-input-group__button--step-down sap-icon--slim-arrow-down'
                               onClick={this.handleDown} />
@@ -99,10 +108,12 @@ export class InputGroup extends Component {
           case 'search':
               return (
                   <div
+                      {...props}
                       className={`fd-input-group${
                           compact ? ' fd-input-group--compact' : ''
                       }`}>
                       <input
+                          {...inputProps}
                           className={`${compact ? 'fd-input fd-input--compact' : ''}`}
                           id={inputId}
                           name={inputName}
@@ -112,6 +123,7 @@ export class InputGroup extends Component {
                           value={this.state.searchValue} />
                       <span className='fd-input-group__addon fd-input-group__addon--button'>
                           <button
+                              {...searchButtonProps}
                               aria-label='Clear'
                               className='fd-input-group__button fd-input-group__button--clear'
                               onClick={this.handleClear} />
@@ -123,6 +135,7 @@ export class InputGroup extends Component {
               if (addonPos === 'before') {
                   return (
                       <div
+                          {...props}
                           className={`fd-input-group fd-input-group--before${
                               compact ? ' fd-input-group--compact' : ''
                           }`}>
@@ -142,6 +155,7 @@ export class InputGroup extends Component {
                               </span>
                           )}
                           <input
+                              {...inputProps}
                               className={`${compact ? 'fd-input fd-input--compact' : ''}`}
                               id={inputId}
                               name={inputName}
@@ -153,10 +167,12 @@ export class InputGroup extends Component {
               } else {
                   return (
                       <div
+                          {...props}
                           className={`fd-input-group fd-input-group--after${
                               compact ? ' fd-input-group--compact' : ''
                           }`}>
                           <input
+                              {...inputProps}
                               className={`${compact ? 'fd-input fd-input--compact' : ''}`}
                               id={inputId}
                               name={inputName}
@@ -196,6 +212,10 @@ InputGroup.propTypes = {
     inputId: PropTypes.string,
     inputName: PropTypes.string,
     inputPlaceholder: PropTypes.string,
+    inputProps: PropTypes.object,
     inputType: PropTypes.oneOf(['text', 'number', 'search']),
-    inputValue: PropTypes.any
+    inputValue: PropTypes.any,
+    numberDownButtonProps: PropTypes.object,
+    numberUpButtonProps: PropTypes.object,
+    searchButtonProps: PropTypes.object
 };
