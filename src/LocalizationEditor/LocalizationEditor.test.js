d57c8bc127a846a56013a6ec20d7394fc1379a93
Greg Smith <greg.smith03@sap.com>
2019-01-18 11:46:44 -0600

    fix: Prop spreading for LocalizationEditor (#258)

--- src/LocalizationEditor/LocalizationEditor.test.js
+++ src/LocalizationEditor/LocalizationEditor.test.js
@@ -4,6 +4,12 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 
 describe('<LocalizationEditor />', () => {
+    const defaultControl = {
+        label: 'Label',
+        placeholder: 'Enter Label',
+        language: 'EN*'
+    };
+
     const defaultMenuArray = [
         { placeholder: 'Enter Label', language: 'ES' },
         { placeholder: 'Enter Label', language: 'CH' },
@@ -64,11 +70,7 @@ describe('<LocalizationEditor />', () => {
         test('should allow props to be spread to the LocalizationEditor component', () => {
             const element = mount(
                 <LocalizationEditor
-                    control={{
-                        label: 'Label',
-                        placeholder: 'Enter Label',
-                        language: 'EN*'
-                    }}
+                    control={defaultControl}
                     data-sample='Sample'
                     menu={defaultMenuArray} />
             );
@@ -78,24 +80,189 @@ describe('<LocalizationEditor />', () => {
             ).toBe('Sample');
         });
 
-        xtest('should allow props to be spread to the LocalizationEditor component\'s Popover component', () => {
-            // TODO: placeholder for this test description once that functionality is built
+        test('should allow props to be spread to the LocalizationEditor component\'s Popover component', () => {
+            const element = mount(
+                <LocalizationEditor
+                    control={defaultControl}
+                    menu={defaultMenuArray}
+                    popoverProps={{
+                        'data-sample': 'Sample'
+                    }} />
+            );
+
+            expect(
+                element.find('.fd-popover').getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
         });
 
-        xtest('should allow props to be spread to the LocalizationEditor component\'s input/textarea element', () => {
-            // TODO: placeholder for this test description once that functionality is built
+        test('should allow props to be spread to the LocalizationEditor component\'s control textarea element', () => {
+            const control = {
+                ...defaultControl,
+                inputProps: {
+                    'data-sample': 'Sample',
+                    id: 'controlTextArea'
+                }
+            };
+            const element = mount(
+                <LocalizationEditor
+                    control={control}
+                    menu={defaultMenuArray}
+                    textarea />
+            );
+
+            expect(
+                element.find('#controlTextArea').getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
         });
 
-        xtest('should allow props to be spread to the LocalizationEditor component\'s label element', () => {
-            // TODO: placeholder for this test description once that functionality is built
+        test('should allow props to be spread to the LocalizationEditor component\'s control input element', () => {
+            const control = {
+                ...defaultControl,
+                inputProps: {
+                    'data-sample': 'Sample',
+                    id: 'controlInput'
+                }
+            };
+            const element = mount(
+                <LocalizationEditor
+                    control={control}
+                    menu={defaultMenuArray} />
+            );
+
+            expect(
+                element.find('#controlInput').getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
         });
 
-        xtest('should allow props to be spread to the LocalizationEditor component\'s button element', () => {
-            // TODO: placeholder for this test description once that functionality is built
+        test('should allow props to be spread to the LocalizationEditor component\'s control label element', () => {
+            const control = {
+                ...defaultControl,
+                labelProps: {
+                    'data-sample': 'Sample'
+                }
+            };
+            const element = mount(
+                <LocalizationEditor
+                    control={control}
+                    menu={defaultMenuArray} />
+            );
+
+            expect(
+                element.find('label').getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
         });
 
-        xtest('should allow props to be spread to the LocalizationEditor component\'s ul element', () => {
-            // TODO: placeholder for this test description once that functionality is built
+        test('should allow props to be spread to the LocalizationEditor component\'s control button element', () => {
+            const control = {
+                ...defaultControl,
+                buttonProps: {
+                    'data-sample': 'Sample',
+                    id: 'controlButton'
+                }
+            };
+            const element = mount(
+                <LocalizationEditor
+                    control={control}
+                    menu={defaultMenuArray} />
+            );
+
+            expect(
+                element.find('#controlButton').getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
+        });
+
+        test('should allow props to be spread to the LocalizationEditor component\'s body ul element', () => {
+            const element = mount(
+                <LocalizationEditor
+                    control={defaultControl}
+                    listProps={{
+                        'data-sample': 'Sample'
+                    }}
+                    menu={defaultMenuArray} />
+            );
+
+            expect(
+                element.find('nav > ul').getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample');
+        });
+
+        test('should allow props to be spread to the LocalizationEditor component\'s body list li elements', () => {
+            const menu = [
+                { placeholder: 'Enter Label', language: 'ES', 'data-sample': 'Sample 1' },
+                { placeholder: 'Enter Label', language: 'CH', 'data-sample': 'Sample 2' },
+                { placeholder: 'Enter Label', language: 'PL', 'data-sample': 'Sample 3' }
+            ];
+            const element = mount(
+                <LocalizationEditor
+                    control={defaultControl}
+                    menu={menu} />
+            );
+
+            const listItems = element.find('nav > ul > li');
+
+            expect(listItems).toHaveLength(3);
+            expect(
+                listItems.at(0).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 1');
+            expect(
+                listItems.at(1).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 2');
+            expect(
+                listItems.at(2).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 3');
+        });
+
+        test('should allow props to be spread to the LocalizationEditor component\'s body list textarea elements', () => {
+            const menu = [
+                { placeholder: 'Enter Label', language: 'ES', inputProps: { 'data-sample': 'Sample 1' } },
+                { placeholder: 'Enter Label', language: 'CH', inputProps: { 'data-sample': 'Sample 2' } },
+                { placeholder: 'Enter Label', language: 'PL', inputProps: { 'data-sample': 'Sample 3' } }
+            ];
+            const element = mount(
+                <LocalizationEditor
+                    control={defaultControl}
+                    menu={menu}
+                    textarea />
+            );
+
+            const listItems = element.find('nav ul li textarea');
+
+            expect(listItems).toHaveLength(3);
+            expect(
+                listItems.at(0).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 1');
+            expect(
+                listItems.at(1).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 2');
+            expect(
+                listItems.at(2).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 3');
+        });
+
+        test('should allow props to be spread to the LocalizationEditor component\'s body list input elements', () => {
+            const menu = [
+                { placeholder: 'Enter Label', language: 'ES', inputProps: { 'data-sample': 'Sample 1' } },
+                { placeholder: 'Enter Label', language: 'CH', inputProps: { 'data-sample': 'Sample 2' } },
+                { placeholder: 'Enter Label', language: 'PL', inputProps: { 'data-sample': 'Sample 3' } }
+            ];
+            const element = mount(
+                <LocalizationEditor
+                    control={defaultControl}
+                    menu={menu} />
+            );
+
+            const listItems = element.find('nav ul li input');
+
+            expect(listItems).toHaveLength(3);
+            expect(
+                listItems.at(0).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 1');
+            expect(
+                listItems.at(1).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 2');
+            expect(
+                listItems.at(2).getDOMNode().attributes['data-sample'].value
+            ).toBe('Sample 3');
         });
     });
 });
