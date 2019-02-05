4133057746c3161348f59b9c50c5262d74c39960
bullman <brad.ullman@sap.com>
2019-02-05 15:36:18 -0800

    lint: no-trailing-spaces

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -185,8 +185,7 @@ module.exports = {
           {
             options: {
               formatter: require.resolve('react-dev-utils/eslintFormatter'),
-              eslintPath: require.resolve('eslint'),
-              
+              eslintPath: require.resolve('eslint')
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -219,7 +218,6 @@ module.exports = {
               customize: require.resolve(
                 'babel-preset-react-app/webpack-overrides'
               ),
-              
               plugins: [
                 [
                   require.resolve('babel-plugin-named-asset-import'),
@@ -259,7 +257,7 @@ module.exports = {
               cacheDirectory: true,
               // Don't waste time on Gzipping the cache
               cacheCompression: false,
-              
+
               // If an error happens in a package, it's possible to be
               // because it was compiled. Thus, we don't want the browser
               // debugger to show the original code. Instead, the code
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -259,8 +259,7 @@ module.exports = {
           {
             options: {
               formatter: require.resolve('react-dev-utils/eslintFormatter'),
-              eslintPath: require.resolve('eslint'),
-              
+              eslintPath: require.resolve('eslint')
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -293,7 +292,6 @@ module.exports = {
               customize: require.resolve(
                 'babel-preset-react-app/webpack-overrides'
               ),
-              
               plugins: [
                 [
                   require.resolve('babel-plugin-named-asset-import'),
@@ -331,7 +329,7 @@ module.exports = {
               cacheDirectory: true,
               // Save disk space when time isn't as important
               cacheCompression: true,
-              
+
               // If an error happens in a package, it's possible to be
               // because it was compiled. Thus, we don't want the browser
               // debugger to show the original code. Instead, the code
