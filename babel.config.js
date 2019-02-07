fb64b105fdca7b23d6bd38fffe801134ab8e7fc7
jbadan <j.badanowski@sap.com>
2019-02-06 17:02:55 -0800

    remove comment

--- babel.config.js
+++ babel.config.js
@@ -21,8 +21,8 @@ const esPresets = [
 
 const defaultPlugins = [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-object-rest-spread', { loose: true }]
-    // '@babel/plugin-transform-object-assign'
+    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+    '@babel/plugin-transform-object-assign'
 ];
 
 const productionPlugins = [
