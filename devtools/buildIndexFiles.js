ff2bd5ecc499ceb9afe02d57df60e1b0c5490ea0
Dylan Moberg <dylan.moberg@concur.com>
2019-02-07 18:44:43 -0800

    programatically generate index files

---
+++ devtools/buildIndexFiles.js
@@ -0,0 +1,32 @@
+const { lstatSync, readdirSync, writeFileSync } = require('fs');
+const path = require('path');
+
+const libPath = path.join(__dirname, '../src');
+
+const isComponentDirectory = (source) => {
+    const ignoredDirectories = ['utils', 'playground', 'ContextualMenu'];
+    return lstatSync(source).isDirectory() && !ignoredDirectories.some(ignored => source.includes(ignored));
+};
+
+const isComponentFile = (source) => {
+    const ignoredFiles = ['test', 'Component', 'index', 'snapshots'];
+    return !ignoredFiles.some(ignored => source.includes(ignored));
+};
+
+
+const componentDirs = readdirSync(libPath).map(name => path.join(libPath, name)).filter(isComponentDirectory).map(directory => {
+    return {
+        path: directory,
+        fileName: readdirSync(directory).filter(isComponentFile)
+    };
+});
+
+componentDirs.map((directory) => {
+    let component = require(`${directory.path}/${directory.fileName}`);
+    let fileContents = '';
+    Object.keys(component).map((single) => {
+        fileContents += `export { ${single} } from './${directory.fileName}';\n`;
+        let indexPath = `${directory.path}/index.js`;
+        writeFileSync(indexPath, fileContents);
+    });
+});
