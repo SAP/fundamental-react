41eeb28e6c036c99d651b24f12b79a28bc36c5a6
Sam Kvale <sam.kvale@sap.com>
2019-02-11 11:52:26 -0600

    fix/icon-example-rendering

--- src/Icon/Icon.Component.js
+++ src/Icon/Icon.Component.js
@@ -633,14 +633,18 @@ export const IconComponent = () => {
         'zoom-out'
     ];
 
-    let icons = listOfIcons.map((icon, index) => {
-        return (
-            <div className='demo-icon-wrapper' key={index}>
-                <Icon glyph={icon} size='xl' />
-                <h5>sap-icon--{icon}</h5>
-            </div>
-        );
-    });
+    let icons = (
+        <div className='fd-doc__margin--icon'>
+            {listOfIcons.map((icon, index) => {
+                return (
+                    <div className='demo-icon-wrapper' key={index}>
+                        <Icon glyph={icon} size='xl' />
+                        <h5>sap-icon--{icon}</h5>
+                    </div>
+                );
+            })}
+        </div>
+    );
 
     return (
         <div>
@@ -674,7 +678,7 @@ export const IconComponent = () => {
 
             <h2>Available Icons</h2>
 
-            <DocsTile>{icons}</DocsTile>
+            <DocsTile centered>{icons}</DocsTile>
 
             <Separator />
 
