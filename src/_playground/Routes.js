202f814386231a29d9f50e36ff5b0838acfd42ec
Greg Smith <greg.smith03@sap.com>
2019-02-18 21:31:04 -0600

    Added RouteNotFound back

--- src/_playground/Routes.js
+++ src/_playground/Routes.js
@@ -10,6 +10,7 @@ import { DatePickerComponent } from '../DatePicker/DatePicker.Component';
 import { DropdownComponent } from '../Dropdown/Dropdown.Component';
 import { FormsComponent } from '../Forms/Forms.Component';
 import groupArray from 'group-array';
+import { Header } from './documentation/Header/Header';
 import { Home } from './documentation/Home/Home';
 import { IconComponent } from '../Icon/Icon.Component';
 import { IdentifierComponent } from '../Identifier/Identifier.Component';
@@ -37,7 +38,7 @@ import { TimePickerComponent } from '../TimePicker/TimePicker.Component';
 import { ToggleComponent } from '../Toggle/Toggle.Component';
 import { TokenComponent } from '../Token/Token.Component';
 import { TreeComponent } from '../Tree/Tree.Component';
-import { BrowserRouter, NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react';
 
 const sections = [
@@ -273,6 +274,13 @@ const routes = [
     }
 ];
 
+const RouteNotFound = () => (
+    <div>
+        <Header>Sorry, page not found.</Header>
+        <Link to=''>Home</Link>
+    </div>
+);
+
 export class Routes extends Component {
     constructor(props) {
         super(props);
@@ -360,6 +368,7 @@ export class Routes extends Component {
                             })}
                             <Redirect exact from=''
                                 to='/home' />
+                            <Route component={RouteNotFound} />
                         </Switch>
                     </div>
                 </div>
@@ -367,4 +376,3 @@ export class Routes extends Component {
         );
     }
 }
-
