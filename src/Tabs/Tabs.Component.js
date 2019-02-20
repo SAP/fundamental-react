4e970a260a50a10e285309313dea536114d16139
Christopher Manciero <christopher.manciero@sap.com>
2019-02-20 10:29:15 -0500

    fix: tabs component refactor (#414)
    * fix: Refactor Tabs component, separated controls
    
    * updated spreading props
    
    * converted TabGroup to React class component
    
    * added changes based of code review
    
    * update prop descriptions
    
    * update component based on code review
    
    * removed unneeded testing code, update unit tests
    
    * changes based on code review
    
    * Minor prop description edits

--- src/Tabs/Tabs.Component.js
+++ src/Tabs/Tabs.Component.js
@@ -1,94 +1,25 @@
 import path from 'path';
 import React from 'react';
+import { Tab } from './Tab';
+import { TabGroup } from './TabGroup';
 import { Description, DocsText, DocsTile, Header, Import, Properties, Separator } from '../_playground';
-import { Link, MemoryRouter } from 'react-router-dom';
-import { Tab, TabGroup } from '../';
 
 export const TabsComponent = () => {
     const tabGroupCode = `
-    <TabGroup
-        selected='1'>
-        <Tab
-            content='Hello world'
-            id='1'
-            url='#'>
-            Tab 1
+    <TabGroup>
+        <Tab id='1' title='Tab 1'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </Tab>
-        <Tab
-            content='Hello world 2'
-            id='2'
-            url='#'>
-            Tab 2
+        <Tab id='2' title='Tab 2'>
+            Numquam libero id corporis odit animi voluptat, Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia tempore eligendi tempora repellat officia rerum laudantium, veritatis officiis asperiores ipsum nam, distinctio, dolor provident culpa voluptatibus esse deserunt animi?
         </Tab>
-        <Tab
-            content='Hello world 3'
-            disabled
-            id='3'
-            url='#'>
-            Tab 3
+        <Tab disabled id='3' title='Tab 3'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </Tab>
+        <Tab glyph='cart' id='4'>
+            Please review your shopping chart.
         </Tab>
     </TabGroup>`;
-
-    const tabsGroupWithAnchorCode = `
-    <TabGroup
-        selected='1'>
-        <Tab
-            content='Hello world'
-            id='1'>
-            <a
-                href='#'>
-                Tab 1
-            </a>
-        </Tab>
-        <Tab
-            content='Hello world 2'
-            id='2'>
-            <a
-                href='#'>
-                Tab 2
-            </a>
-        </Tab>
-        <Tab
-            content='Hello world 3'
-            disabled
-            id='3'>
-            <a
-                href='#'>
-                Tab 2
-            </a>
-        </Tab>
-    </TabGroup>`;
-
-    const tabsGroupWithLinkCode = `
-    <MemoryRouter>
-        <TabGroup>
-            <Tab
-                content='Hello world'
-                id='1'>
-                <Link
-                    to='/'>
-                    Tab 1
-                </Link>
-            </Tab>
-            <Tab
-                content='Hello world 2'
-                id='2'>
-                <Link
-                    to='/'>
-                    Tab 2
-                </Link>
-            </Tab>
-            <Tab
-                content='Hello world 3'
-                disabled
-                id='3'>
-                <Link
-                    to='/'>
-                    Tab 2
-                </Link>
-            </Tab>
-        </TabGroup>
-    </MemoryRouter>`;
 
     return (
         <div>
@@ -98,115 +29,36 @@ export const TabsComponent = () => {
                 metaphor and is used to separate content into different sections.
                 They should be ordered to create a visual hierarchy based on priority.
             </Description>
-            <Import sourceModulePath={path.join(__dirname, './Tabs')} />
+            <Import sourceModulePath={path.join(__dirname, '../Tabs/TabGroup')} />
+            <Import sourceModulePath={path.join(__dirname, '../Tabs/Tab')} />
 
             <Separator />
 
-            <Properties sourceModulePath={path.join(__dirname, './Tabs')} />
+            <Properties sourceModulePath={path.join(__dirname, '../Tabs/TabGroup')} />
+            <Properties sourceModulePath={path.join(__dirname, '../Tabs/Tab')} />
 
             <Separator />
 
-            <h2>Tab Group with URL</h2>
+            <h2>Tab Group</h2>
 
             <DocsTile>
-                <TabGroup
-                    selectedId='1'>
-                    <Tab
-                        content='Hello world'
-                        id='1'
-                        url='#'>
-                        Tab 1
+                <TabGroup>
+                    <Tab id='1' title='Tab 1'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     </Tab>
-                    <Tab
-                        content='Hello world 2'
-                        id='2'
-                        url='#'>
-                        Tab 2
+                    <Tab id='2' title='Tab 2'>
+                        Numquam libero id corporis odit animi voluptat, Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia tempore eligendi tempora repellat officia rerum laudantium, veritatis officiis asperiores ipsum nam, distinctio, dolor provident culpa voluptatibus esse deserunt animi?
                     </Tab>
-                    <Tab
-                        content='Hello world 3'
-                        disabled
-                        id='3'
-                        url='#'>
-                        Tab 3
+                    <Tab disabled id='3'
+                        title='Tab 3'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </Tab>
+                    <Tab glyph='cart' id='4'>
+                        Please review your shopping chart.
                     </Tab>
                 </TabGroup>
             </DocsTile>
             <DocsText>{tabGroupCode}</DocsText>
-
-            <Separator />
-
-            <h2>Tab Group with Anchor</h2>
-
-            <DocsTile>
-                <TabGroup
-                    selectedId='1'>
-                    <Tab
-                        content='Hello world'
-                        id='1'>
-                        <a
-                            href='#'>
-                            Tab 1
-                        </a>
-                    </Tab>
-                    <Tab
-                        content='Hello world 2'
-                        id='2'>
-                        <a
-                            href='#'>
-                            Tab 2
-                        </a>
-                    </Tab>
-                    <Tab
-                        content='Hello world 3'
-                        disabled
-                        id='3'>
-                        <a
-                            href='#'>
-                            Tab 2
-                        </a>
-                    </Tab>
-                </TabGroup>
-            </DocsTile>
-            <DocsText>{tabsGroupWithAnchorCode}</DocsText>
-
-            <Separator />
-
-            <h2>Tab Group with Link</h2>
-
-            <DocsTile>
-                <MemoryRouter>
-                    <TabGroup
-                        selectedId='1'>
-                        <Tab
-                            content='Hello world'
-                            id='1'>
-                            <Link
-                                to='/'>
-                                Tab 1
-                            </Link>
-                        </Tab>
-                        <Tab
-                            content='Hello world 2'
-                            id='2'>
-                            <Link
-                                to='/'>
-                                Tab 2
-                            </Link>
-                        </Tab>
-                        <Tab
-                            content='Hello world 3'
-                            disabled
-                            id='3'>
-                            <Link
-                                to='/'>
-                                Tab 2
-                            </Link>
-                        </Tab>
-                    </TabGroup>
-                </MemoryRouter>
-            </DocsTile>
-            <DocsText>{tabsGroupWithLinkCode}</DocsText>
         </div>
     );
 };
