fb7a56dad2fa3f37abb7268cb548cd389c197e4f
jbadan <jenna.badanowski@gmail.com>
2019-02-25 09:23:42 -0800

    remove label

--- src/Badge/Badge.js
+++ src/Badge/Badge.js
@@ -1,7 +1,7 @@
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { BADGE_MODIFIERS, BADGE_TYPES, LABEL_TYPES, STATUS_TYPES } from '../utils/constants';
+import { BADGE_MODIFIERS, BADGE_TYPES, STATUS_TYPES } from '../utils/constants';
 
 
 export const Badge = ({ type, modifier, children, className, ...props }) => {
@@ -26,24 +26,6 @@ Badge.propTypes = {
     className: PropTypes.string,
     modifier: PropTypes.oneOf(BADGE_MODIFIERS),
     type: PropTypes.oneOf(BADGE_TYPES)
-};
-
-export const Label = ({ type, children, className, ...props }) => {
-    const labelClasses = classnames(
-        'fd-label',
-        {
-            [`fd-label--${type}`]: !!type
-        },
-        className
-    );
-
-    return <span {...props} className={labelClasses}>{children}</span>;
-};
-Label.displayName = 'Label';
-
-Label.propTypes = {
-    className: PropTypes.string,
-    type: PropTypes.oneOf(LABEL_TYPES)
 };
 
 export const Status = ({ type, glyph, children, className, ...props }) => {
