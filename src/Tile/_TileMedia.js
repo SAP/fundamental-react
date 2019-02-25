8741107a653530b4693cf5edf96fc708db9334d8
Greg Smith <greg.smith03@sap.com>
2019-02-25 14:55:36 -0600

    fix: One component per file for Tile (#441)
    * Rename file
    
    * Rename temp
    
    * Restore original
    
    * Rename file
    
    * Rename temp
    
    * Restore original
    
    * Rename file
    
    * Rename temp
    
    * Restore original
    
    * Rename file
    
    * Rename temp
    
    * Restore original
    
    * Rename file
    
    * Rename temp
    
    * Restore original
    
    * Rename file
    
    * Rename temp
    
    * Restore original
    
    * Rename file
    
    * Rename temp
    
    * Restore original
    
    * Changes to refactor Tile into separate source files
    
    * Remove script file
    
    * Fixed issues in playground
    
    * Fixed issues in Panel docs page

---
+++ src/Tile/_TileMedia.js
@@ -0,0 +1,23 @@
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const TileMedia = props => {
+    const { children, className, ...rest } = props;
+
+    const tileMediaClasses = classnames(
+        'fd-tile__media',
+        className
+    );
+
+    return <div {...rest} className={tileMediaClasses}>{children}</div>;
+};
+
+TileMedia.displayName = 'TileMedia';
+
+TileMedia.propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string
+};
+
+export default TileMedia;
