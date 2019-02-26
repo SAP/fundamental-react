f32bae2941a85d7c3a4b2aa91ecb231775126fed
bullman <brad.ullman@sap.com>
2019-02-25 20:11:50 -0800

    remove collectCoverageFrom block

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -4,11 +4,5 @@ import { configure } from 'enzyme';
 configure({ adapter: new Adapter() });
 
 module.exports = {
-    'testURL': 'http://localhost/',
-    'collectCoverageFrom': [
-        '!config/**/*.{js,jsx}',
-        'src/**/*.{js,jsx}',
-        '!src/**/*.Component.js',
-        '!src/_playground/**'
-    ]
+    'testURL': 'http://localhost/'
 };
