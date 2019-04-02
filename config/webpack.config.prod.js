9a38cba65df49da417fd0d4af0b1ac48de38997e
jbadan <jenna.badanowski@gmail.com>
2019-04-02 15:13:02 -0700

    update webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -128,7 +128,7 @@ module.exports = {
         filename: 'static/js/[name].[chunkhash:8].js',
         chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
         // We inferred the "public path" (such as / or /my-project) from homepage.
-        publicPath: publicPath,
+        publicPath: process.env.NETLITFY ? '' : publicPath,
         // Point sourcemap entries to original disk location (format as URL on Windows)
         devtoolModuleFilenameTemplate: info =>
             path
