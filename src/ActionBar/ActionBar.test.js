ee351c690270a71bdaa8869b33df3c41f22bf085
hannah.van.seeters <hannah.van.seeters@sap.com>
2019-05-03 08:22:16 -0700

    rearranged tests for actionBarBack

--- src/ActionBar/ActionBar.test.js
+++ src/ActionBar/ActionBar.test.js
@@ -91,23 +91,6 @@ describe('<ActionBar />', () => {
             ).toBe('Sample');
         });
 
-        test('should allow props to be spread to the ActionBarBack component', () => {
-            const element = mount(<ActionBar.Back data-sample='Sample' />);
-
-            expect(
-                element.getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
-        test('should allow props to be spread to the ActionBarBack component\'s button element', () => {
-
-            const element = mount(<ActionBar.Back buttonProps={{ 'data-sample': 'Sample' }} />);
-
-            expect(
-                element.find('button').getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
         test('should allow props to be spread to the ActionBarActions component', () => {
             const element = mount(<ActionBar.Actions data-sample='Sample' />);
 
--- src/ActionBar/_ActionBarBack.test.js
+++ src/ActionBar/_ActionBarBack.test.js
@@ -1,119 +1,23 @@
 import ActionBar from './ActionBar';
 import { mount } from 'enzyme';
 import React from 'react';
-import renderer from 'react-test-renderer';
 
-describe('<ActionBar />', () => {
-    const basicActionBar = (
-        <ActionBar className='blue'>
-            <ActionBar.Back className='blue' />
-            <ActionBar.Header
-                className='blue'
-                description={'Action Bar Description'}
-                title={'Page Title'} />
-            <ActionBar.Actions className='blue'>
-                <button>Button</button>
-            </ActionBar.Actions>
-        </ActionBar>
-    );
+describe('ActionBarBack Prop spreading', () => {
+    test('should allow props to be spread to the ActionBarBack component', () => {
+        const element = mount(<ActionBar.Back data-sample='Sample' />);
 
-    const basicActionBarNoClass = (
-        <ActionBar>
-            <ActionBar.Back className='blue' />
-            <ActionBar.Header
-                className='blue'
-                description={'Action Bar Description'}
-                title={'Page Title'} />
-            <ActionBar.Actions className='blue'>
-                <button>Button</button>
-            </ActionBar.Actions>
-        </ActionBar>
-    );
-
-    const mobileActionBar = (
-        <ActionBar className='blue' mobile>
-            <ActionBar.Back />
-            <ActionBar.Header
-                description={'Action Bar Description'}
-                title={'Page Title'} />
-            <ActionBar.Actions>
-                <button>Button</button>
-            </ActionBar.Actions>
-        </ActionBar>
-    );
-
-    const mobileActionBarWidthSet = (
-        <ActionBar mobile width='500px'>
-            <ActionBar.Back />
-            <ActionBar.Header
-                description={'Action Bar Description'}
-                title={'Page Title'} />
-            <ActionBar.Actions>
-                <button>Button</button>
-            </ActionBar.Actions>
-        </ActionBar>
-    );
-
-    test('create basic Action Bar', () => {
-        let component = renderer.create(basicActionBar);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-
-        component = renderer.create(basicActionBarNoClass);
-        tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+        expect(
+            element.getDOMNode().attributes['data-sample'].value
+        ).toBe('Sample');
     });
 
-    test('create basic mobile Action Bar', () => {
-        let component = renderer.create(mobileActionBar);
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+    test('should allow props to be spread to the ActionBarBack component\'s button element', () => {
 
-        component = renderer.create(mobileActionBarWidthSet);
-        tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
-    });
+        const element = mount(<ActionBar.Back buttonProps={{ 'data-sample': 'Sample' }} />);
 
-    describe('Prop spreading', () => {
-        test('should allow props to be spread to the ActionBar component', () => {
-            const element = mount(<ActionBar data-sample='Sample' />);
-
-            expect(
-                element.getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
-        test('should allow props to be spread to the ActionBar component for mobile', () => {
-            const element = mount(<ActionBar data-sample='Sample' mobile />);
-
-            expect(
-                element.getDOMNode().querySelector('div').attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
-        test('should allow props to be spread to the ActionBarBack component', () => {
-            const element = mount(<ActionBar.Back data-sample='Sample' />);
-
-            expect(
-                element.getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
-        test('should allow props to be spread to the ActionBarBack component\'s button element', () => {
-
-            const element = mount(<ActionBar.Back buttonProps={{ 'data-sample': 'Sample' }} />);
-
-            expect(
-                element.find('button').getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
-        test('should allow props to be spread to the ActionBarActions component', () => {
-            const element = mount(<ActionBar.Actions data-sample='Sample' />);
-
-            expect(
-                element.getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
+        expect(
+            element.find('button').getDOMNode().attributes['data-sample'].value
+        ).toBe('Sample');
     });
 });
+
