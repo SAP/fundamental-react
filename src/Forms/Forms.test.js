ce9fa547528c110a20f8b8ecb903cc2d60d3ef9d
hannah.van.seeters <hannah.van.seeters@sap.com>
2019-05-03 10:48:44 -0700

    split tests for formSet

--- src/Forms/Forms.test.js
+++ src/Forms/Forms.test.js
@@ -237,14 +237,6 @@ describe('<Forms />', () => {
     });
 
     describe('Prop spreading', () => {
-        test('should allow props to be spread to the FormSet component', () => {
-            const element = mount(<FormSet data-sample='Sample' />);
-
-            expect(
-                element.getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
         test('should allow props to be spread to the FormTextarea component', () => {
             const element = mount(<FormTextarea data-sample='Sample' />);
 
