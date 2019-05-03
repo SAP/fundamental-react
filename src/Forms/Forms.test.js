bcfa547aa74e3c69b203a896bbc7a981c3abbd99
hannah.van.seeters <hannah.van.seeters@sap.com>
2019-05-03 09:08:40 -0700

    rearranged tests for FormFieldset

--- src/Forms/Forms.test.js
+++ src/Forms/Forms.test.js
@@ -237,14 +237,6 @@ describe('<Forms />', () => {
     });
 
     describe('Prop spreading', () => {
-        test('should allow props to be spread to the FormFieldset component', () => {
-            const element = mount(<FormFieldset data-sample='Sample' />);
-
-            expect(
-                element.getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
         test('should allow props to be spread to the FormInput component', () => {
             const element = mount(<FormInput data-sample='Sample' />);
 
