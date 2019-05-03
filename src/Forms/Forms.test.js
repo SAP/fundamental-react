ff79fe44fa17ebf6da4d8e5b41a7fe1e84859f90
hannah.van.seeters <hannah.van.seeters@sap.com>
2019-05-03 09:19:55 -0700

    split FormItem tests

--- src/Forms/Forms.test.js
+++ src/Forms/Forms.test.js
@@ -237,14 +237,6 @@ describe('<Forms />', () => {
     });
 
     describe('Prop spreading', () => {
-        test('should allow props to be spread to the FormItem component', () => {
-            const element = mount(<FormItem data-sample='Sample' />);
-
-            expect(
-                element.getDOMNode().attributes['data-sample'].value
-            ).toBe('Sample');
-        });
-
         test('should allow props to be spread to the FormLabel component', () => {
             const element = mount(<FormLabel data-sample='Sample' />);
 
