09da1ecee73a1325ba1a343e93d4a090e9e8dad9
bullman <brad.ullman@sap.com>
2019-06-28 18:11:09 -0700

    remove react and react dom from size limit calc

--- config/webpack.config.sizeLimit.js
+++ config/webpack.config.sizeLimit.js
@@ -33,6 +33,20 @@ module.exports = {
             }
         ]
     },
+    externals: {
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'React',
+            root: 'React'
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'ReactDOM',
+            root: 'ReactDOM'
+        }
+    },
     plugins: [
         new OptimizeCSSAssetsPlugin(),
         new CompressionPlugin(),
