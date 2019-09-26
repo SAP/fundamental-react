912255abee234890ab18ccae3b59d04a41a542a6
jbadan <jenna.badanowski@gmail.com>
2019-09-26 10:23:29 -0700

    remove uneeded describe block

--- src/utils/WithStyles/WithStyles.test.js
+++ src/utils/WithStyles/WithStyles.test.js
@@ -17,51 +17,49 @@ TestComponent.SubComponent = _SubTestComponent;
 
 export default withStyles(TestComponent, { cssFile: 'foo', fonts: true });
 
-describe('<withStyles />', () => {
-    describe('default behavior with no customization', () => {
-        const wrapper = mount(<TestComponent foo='foo' />);
+describe('<withStyles', () => {
+    const wrapper = mount(<TestComponent foo='foo' />);
 
-        test('should hoist the wrapped component\'s static references', () => {
-            expect(TestComponent.SubComponent).toBeDefined();
-        });
+    test('should hoist the wrapped component\'s static references', () => {
+        expect(TestComponent.SubComponent).toBeDefined();
+    });
 
-        test('should retain wrapped component\'s display name', () => {
-            expect(wrapper.name()).toBe('TestComponent');
-        });
+    test('should retain wrapped component\'s display name', () => {
+        expect(wrapper.name()).toBe('TestComponent');
+    });
 
-        test('should forward wrapped component\'s props', () => {
-            expect(wrapper.props().foo).toBe('foo');
-        });
+    test('should forward wrapped component\'s props', () => {
+        expect(wrapper.props().foo).toBe('foo');
+    });
 
-        test('should forward the wrapped functional component\'s ref', () => {
-            let ref;
-            class Test extends React.Component {
-                constructor(props) {
-                    super(props);
-                    ref = React.createRef();
-                }
+    test('should forward the wrapped functional component\'s ref', () => {
+        let ref;
+        class Test extends React.Component {
+            constructor(props) {
+                super(props);
+                ref = React.createRef();
+            }
             render = () => <TestComponent ref={ref}>Button</TestComponent>;
+        }
+        mount(<Test />);
+        expect(ref.current.tagName).toEqual('DIV');
+    });
+    test('should forward the wrapped class component\'s ref', () => {
+        class TargetComponent extends React.Component {
+            render() {
+                return null;
             }
-            mount(<Test />);
-            expect(ref.current.tagName).toEqual('DIV');
-        });
-        test('should forward the wrapped class component\'s ref', () => {
-            class TargetComponent extends React.Component {
-                render() {
-                    return null;
-                }
-            }
+        }
 
-            const StyledTarget = withStyles(TargetComponent);
-            const ref = React.createRef();
-            mount(
-                <React.Fragment>
-                    <StyledTarget ref={ref} />
-                </React.Fragment>
-            );
+        const StyledTarget = withStyles(TargetComponent);
+        const ref = React.createRef();
+        mount(
+            <React.Fragment>
+                <StyledTarget ref={ref} />
+            </React.Fragment>
+        );
 
-            expect(ref.current).toBeInstanceOf(TargetComponent);
+        expect(ref.current).toBeInstanceOf(TargetComponent);
 
-        });
     });
 });
