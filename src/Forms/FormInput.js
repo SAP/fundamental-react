6cb2ec5f50af7e515a8ca6cfd14f78909fbd4d5d
h.van.seeters <h.van.seeters@sap.com>
2019-09-25 21:01:57 -0700

    FormInput

--- src/Forms/FormInput.js
+++ src/Forms/FormInput.js
@@ -1,10 +1,10 @@
-import 'fundamental-styles/dist/input.css';
 import classnames from 'classnames';
 import { INPUT_TYPES } from '../utils/constants';
 import PropTypes from 'prop-types';
 import React from 'react';
+import withStyles from '../utils/WithStyles';
 
-const FormInput = ({ state, className, compact, disabled, id, name, placeholder, readOnly, type, value, ...props }) => {
+const FormInput = React.forwardRef(({ state, className, compact, disabled, id, name, placeholder, readOnly, type, value, customStyles, disableStyles, ...props }, ref) => {
     const formInputClasses = classnames(
         'fd-input',
         {
@@ -23,17 +23,20 @@ const FormInput = ({ state, className, compact, disabled, id, name, placeholder,
             name={name}
             placeholder={placeholder}
             readOnly={readOnly}
+            ref={ref}
             type={type}
             value={value} />
     );
-};
+});
 
 FormInput.displayName = 'FormInput';
 
 FormInput.propTypes = {
     className: PropTypes.string,
     compact: PropTypes.bool,
+    customStyles: PropTypes.object,
     disabled: PropTypes.bool,
+    disableStyles: PropTypes.bool,
     id: PropTypes.string,
     name: PropTypes.string,
     placeholder: PropTypes.string,
@@ -54,4 +57,4 @@ FormInput.propDescriptions = {
     value: 'Value for the `value` attribute on the input.'
 };
 
-export default FormInput;
+export default withStyles(FormInput, { cssFile: 'input', fonts: true });
