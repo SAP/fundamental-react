cc4c05bf2793e43f5d4b54fbfa059808fb5953dc
h.van.seeters <h.van.seeters@sap.com>
2019-09-26 12:10:35 -0700

    Icon, Image, Identifier

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,10 +1,10 @@
-import 'fundamental-styles/dist/icon.css';
 import classnames from 'classnames';
 import { ICON_SIZES } from '../utils/constants';
 import PropTypes from 'prop-types';
 import React from 'react';
+import withStyles from '../utils/WithStyles/WithStyles';
 
-const Icon = ({ glyph, size, className, ...props }) => {
+const Icon = React.forwardRef(({ glyph, size, className, customStyles, disableStyles, ...props }, ref) => {
     const iconClasses = classnames(
         {
             [`sap-icon--${glyph}`]: !!glyph,
@@ -16,15 +16,18 @@ const Icon = ({ glyph, size, className, ...props }) => {
     return (
         <span
             {...props}
-            className={iconClasses} />
+            className={iconClasses}
+            ref={ref} />
     );
-};
+});
 
 Icon.displayName = 'Icon';
 
 Icon.propTypes = {
     glyph: PropTypes.string.isRequired,
     className: PropTypes.string,
+    customStyles: PropTypes.object,
+    disableStyles: PropTypes.bool,
     size: PropTypes.oneOf(ICON_SIZES)
 };
 
@@ -32,4 +35,4 @@ Icon.propDescriptions = {
     size: 'Size of the icon. Options include **xs**, **s**, **compact**, and **l**. If no size is provided, default (normal) will be used.'
 };
 
-export default Icon;
+export default withStyles(Icon, { cssFile: 'icon' });
