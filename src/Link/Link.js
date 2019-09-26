6d2dda2f10832ea8ce4b29faaa45fd14141eec0c
h.van.seeters <h.van.seeters@sap.com>
2019-09-26 13:42:11 -0700

    Link

--- src/Link/Link.js
+++ src/Link/Link.js
@@ -1,9 +1,9 @@
-import 'fundamental-styles/dist/link.css';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import withStyles from '../utils/WithStyles/WithStyles';
 
-const Link = ({ className, children, disabled, ...props }) => {
+const Link = React.forwardRef(({ className, children, disabled, customStyles, disableStyles, ...props }, ref) => {
     const imageClasses = classnames(
         'fd-link',
         { 'is-disabled': !!disabled },
@@ -13,17 +13,22 @@ const Link = ({ className, children, disabled, ...props }) => {
         <a
             {...props}
             aria-disabled={disabled}
-            className={imageClasses}>{children}</a>
+            className={imageClasses}
+            ref={ref}>
+            {children}
+        </a>
     );
-};
+});
 
 Link.displayName = 'Link';
 
 Link.propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
-    disabled: PropTypes.bool
+    customStyles: PropTypes.object,
+    disabled: PropTypes.bool,
+    disableStyles: PropTypes.bool
 };
 
 
-export default Link;
+export default withStyles(Link, { cssFile: 'link', fonts: true });
