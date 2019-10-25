a931091d0013654f262069154ae4b2b8ae98b4ef
Jenna Badanowski <29607818+jbadan@users.noreply.github.com>
2019-10-25 15:04:19 -0700

    fix: add build:copy-files after standard version has run (#777)

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -15,6 +15,8 @@ echo "$std_ver"
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master > /dev/null 2>&1;
 
+npm run build:copy-files
+
 npm publish lib
 
 # run this after publish to make sure GitHub finishes updating from the push
