5d80730f66a87bc6a7c49aebdadc439ff7dc6fdd
jbadan <jenna.badanowski@gmail.com>
2019-12-04 13:00:44 -0800

    lock viewport for ci

--- visual-regression-testing/storyshots.test.js
+++ visual-regression-testing/storyshots.test.js
@@ -1,22 +1,35 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable compat/compat */
+/* eslint-disable no-console */
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import initStoryshots from '@storybook/addon-storyshots';
 
-const getMatchOptions = ({ context: { kind, story }, url }) => {
+const getMatchOptions = () => {
     return {
         failureThreshold: 0.2,
         failureThresholdType: 'percent'
     };
 };
-const beforeScreenshot = (page, { context: { kind, story }, url }) => {
-    return new Promise(resolve =>
-        setTimeout(() => {
-            resolve();
-        }, 100)
-    );
+
+//This is needed to keep CI from failing due to viewport differences
+const view = {
+    name: 'Desktop 800x600',
+    userAgent: 'placeholder',
+    viewport: {
+        width: 800,
+        height: 600
+    }
+};
+
+const beforeScreenshot = (page) => {
+    return page
+        .emulate(view)
+        .then(resp => resp)
+        .catch(ex => console.error(ex));
 };
 
 initStoryshots({
-    test: imageSnapshot({ storybookUrl: 'http://localhost:12123/', getMatchOptions, beforeScreenshot })
+    test: imageSnapshot({
+        storybookUrl: 'http://localhost:12123/',
+        getMatchOptions,
+        beforeScreenshot
+    })
 });
