dc33abfda87aeaae44cdb156c9384257ec461d87
Jacob Devera <jacob.devera@sap.com>
2019-12-17 13:56:46 -0800

    fix: use momentjs for calendar and datepicker (#817)
    * fix: use momentjs
    
    * fix: next/prev handling
    
    * fix: use moment in datepicker
    
    * fix: simplify derived state logic
    
    * fix: simplify enabled date logic
    
    * fix: simplify validation logic
    
    * fix: tests
    
    * fix: localization
    
    * fix: increase size limit
    
    * fix: use date objects for props
    
    * chore: calendar stories
    
    * chore: datepicker stories
    
    * chore: locale
    
    * merge
    
    * fix: unnecessary diffs
    
    * fix: visual regression testing

--- src/Calendar/__stories__/Calendar.stories.js
+++ src/Calendar/__stories__/Calendar.stories.js
@@ -2,13 +2,53 @@ import Calendar from '../Calendar';
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import {
+    boolean,
+    date,
+    optionsKnob,
+    text,
     withKnobs
 } from '@storybook/addon-knobs';
+
+
+function dateKnobToDate(name, defaultValue) {
+    const stringTimestamp = date(name, defaultValue);
+    return new Date(stringTimestamp);
+}
+
+const afterDateDefault = new Date(new Date().getFullYear() + 1, 0, 1);
+const beforeDateDefault = new Date(new Date().getFullYear() - 1, 0, 1);
+
+const blockedDateFirstDefault = new Date(new Date().getFullYear(), new Date().getMonth() - 1, 1);
+const blockedDateSecondDefault = new Date(new Date().getFullYear(), new Date().getMonth() - 1, 10);
+
+const disabledDateFirstDefault = new Date(new Date().getFullYear(), new Date().getMonth() - 1, 15);
+const disabledDateSecondDefault = new Date(new Date().getFullYear(), new Date().getMonth() - 1, 25);
+
+const weekdayOptions = {
+    Sunday: 'Sunday',
+    Monday: 'Monday',
+    Tuesday: 'Tuesday',
+    Wednesday: 'Wednesday',
+    Thursday: 'Thursday',
+    Friday: 'Friday',
+    Saturday: 'Saturday'
+};
 
 storiesOf('Components|Calendar', module)
     .addDecorator(withKnobs)
     .add('Default', () => (
-        <Calendar />
+        <Calendar
+            blockedDates={[dateKnobToDate('block between dates (1)', blockedDateFirstDefault),
+                dateKnobToDate('block between dates (2)', blockedDateSecondDefault)]}
+            disableAfterDate={dateKnobToDate('disable after date', afterDateDefault)}
+            disableBeforeDate={dateKnobToDate('disable before date', beforeDateDefault)}
+            disableFutureDates={boolean('disable future dates', false)}
+            disablePastDates={boolean('disable past dates', false)}
+            disableWeekday={optionsKnob('disable weekdays', weekdayOptions, null, { display: 'check' })}
+            disableWeekends={boolean('disable weekends', false)}
+            disabledDates={[dateKnobToDate('disable between dates (1)', disabledDateFirstDefault),
+                dateKnobToDate('disable between dates (2)', disabledDateSecondDefault)]}
+            locale={text('locale', 'en')} />
     ))
     .add('disable styles', () => (
         <Calendar disableStyles />
@@ -16,4 +56,7 @@ storiesOf('Components|Calendar', module)
     .add('custom styles', () => (
         <Calendar
             customStyles={require('../../utils/WithStyles/customStylesTest.css')} />
+    ))
+    .add('range selection', () => (
+        <Calendar enableRangeSelection />
     ));
