37b5ad911607ff73b3a304f2b3cd3c45ab647649
jbadan <jenna.badanowski@gmail.com>
2020-02-20 14:40:54 -0800

    form message types

--- src/Forms/__stories__/FormMessage.stories.js
+++ src/Forms/__stories__/FormMessage.stories.js
@@ -2,13 +2,25 @@ import FormMessage from '../FormMessage';
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import {
+    select,
     withKnobs
 } from '@storybook/addon-knobs';
+
+const createProps = (overrides) => ({
+    type: select('Types', {
+        'default': null,
+        'error': 'error',
+        'success': 'success',
+        'information': 'information',
+        'warning': 'warning'
+    }),
+    ...overrides
+});
 
 storiesOf('Components|FormMessage', module)
     .addDecorator(withKnobs)
     .add('Default', () => (
-        <FormMessage>Default</FormMessage>
+        <FormMessage {...createProps()}>Default</FormMessage>
     ))
     .add('disable styles', () => (
         <FormMessage disableStyles>Default</FormMessage>
