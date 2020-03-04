508f3a3b8bd6a39c781954828522f3392dcb6c18
jbadan <jenna.badanowski@gmail.com>
2020-03-04 09:15:43 -0800

    update action bar tests

--- src/ActionBar/ActionBar.test.js
+++ src/ActionBar/ActionBar.test.js
@@ -68,7 +68,7 @@ describe('<ActionBar />', () => {
 
     describe('Prop spreading', () => {
         test('should allow props to be spread to the ActionBar component', () => {
-            const element = mount(<ActionBar data-sample='Sample' />);
+            const element = mount(<ActionBar data-sample='Sample' title='Page Title' />);
 
             expect(
                 element.getDOMNode().attributes['data-sample'].value
@@ -76,7 +76,8 @@ describe('<ActionBar />', () => {
         });
 
         test('should allow props to be spread to the back Button component', () => {
-            const element = mount(<ActionBar buttonProps={{ 'data-sample': 'Sample' }} onClick={() => {}} />);
+            const element = mount(<ActionBar buttonProps={{ 'data-sample': 'Sample' }} onBackClick={() => {}}
+                title='Page Title' />);
 
             expect(
                 element.find('.sap-icon--navigation-left-arrow').getDOMNode().attributes['data-sample'].value
@@ -84,7 +85,7 @@ describe('<ActionBar />', () => {
         });
 
         test('should allow props to be spread to the title', () => {
-            const element = mount(<ActionBar titleProps={{ 'data-sample': 'Sample' }} />);
+            const element = mount(<ActionBar title='Page Title' titleProps={{ 'data-sample': 'Sample' }} />);
 
             expect(
                 element.find('.fd-action-bar__title').getDOMNode().attributes['data-sample'].value
@@ -92,7 +93,8 @@ describe('<ActionBar />', () => {
         });
 
         test('should allow props to be spread to the action container', () => {
-            const element = mount(<ActionBar actionProps={{ 'data-sample': 'Sample' }} actions={(<button>Button</button>)} />);
+            const element = mount(<ActionBar actionProps={{ 'data-sample': 'Sample' }} actions={(<button>Button</button>)}
+                title='Page Title' />);
 
             expect(
                 element.find('.fd-action-bar__actions').getDOMNode().attributes['data-sample'].value
@@ -107,7 +109,7 @@ describe('<ActionBar />', () => {
                 super(props);
                 ref = React.createRef();
             }
-            render = () => <ActionBar ref={ref} />;
+            render = () => <ActionBar ref={ref} title='Page Title' />;
         }
         mount(<Test />);
 
