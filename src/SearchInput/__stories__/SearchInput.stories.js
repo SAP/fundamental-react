62ecf96dae90378126dd0c7f3899588031191dbe
jbadan <jenna.badanowski@gmail.com>
2020-05-04 07:36:49 -0700

    searchinput

--- src/SearchInput/__stories__/SearchInput.stories.js
+++ src/SearchInput/__stories__/SearchInput.stories.js
@@ -1,80 +1,76 @@
+/* eslint-disable no-console */
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
 import SearchInput from '../SearchInput';
-import { storiesOf } from '@storybook/react';
 import {
     boolean,
-    select,
-    withKnobs
+    select
 } from '@storybook/addon-knobs';
 
 const searchData = [
-    { text: 'apple', callback: () => alert('apple') },
-    { text: 'apricot', callback: () => alert('apricot') },
-    { text: 'banana', callback: () => alert('banana') },
-    { text: 'blueberry', callback: () => alert('blueberry') },
-    { text: 'blackberry', callback: () => alert('blackberry') },
-    { text: 'calabash', callback: () => alert('calabash') },
-    { text: 'clementines', callback: () => alert('clementines') },
-    { text: 'kiwi', callback: () => alert('kiwi') },
-    { text: 'orange', callback: () => alert('orange') }
+    { text: 'apple', callback: () => console.log('apple') },
+    { text: 'apricot', callback: () => console.log('apricot') },
+    { text: 'banana', callback: () => console.log('banana') },
+    { text: 'blueberry', callback: () => console.log('blueberry') },
+    { text: 'blackberry', callback: () => console.log('blackberry') },
+    { text: 'calabash', callback: () => console.log('calabash') },
+    { text: 'clementines', callback: () => console.log('clementines') },
+    { text: 'kiwi', callback: () => console.log('kiwi') },
+    { text: 'orange', callback: () => console.log('orange') }
 ];
 
-const createProps = (overrides) => ({
-    compact: boolean('compact', false),
-    searchList: searchData,
-    validationState: select('Validation State', {
-        'none': '',
-        'success': { state: 'success', text: 'placeholder text' },
-        'error': { state: 'error', text: 'placeholder text' },
-        'information': { state: 'information', text: 'placeholder text' },
-        'warning': { state: 'warning', text: 'placeholder text' }
-    }),
-    placeholder: 'Select a Fruit',
-    ...overrides
-});
+export default {
+    title: 'Component API/SearchInput',
+    component: SearchInput
+};
 
 
-storiesOf('Components|SearchInput', module)
-    .addDecorator(withKnobs)
-    .add('Default', () => (
-        <SearchInput {...createProps()} />
-    ))
-    .add('Compact', () => (
-        <SearchInput compact
-            placeholder='Placeholder'
-            searchList={searchData} />
-    ))
-    .add('Disabled', () => (
-        <SearchInput disabled
-            placeholder='Placeholder'
-            searchList={searchData} />
-    ))
-    .add('Validation State | Error', () => (
+export const compact = () => (
+    <SearchInput compact
+        placeholder='Placeholder'
+        searchList={searchData} />
+);
+
+export const disabled = () => (
+    <SearchInput disabled
+        placeholder='Placeholder'
+        searchList={searchData} />
+);
+
+export const validationStates = () => (
+    <>
         <SearchInput
-            placeholder='Default'
+            placeholder='Error'
             searchList={searchData}
             validationState={{ state: 'error', text: 'Test validation state' }} />
-    ))
-    .add('Validation State | Warning', () => (
         <SearchInput
-            placeholder='Default'
+            placeholder='Warning'
             searchList={searchData}
             validationState={{ state: 'warning', text: 'Test validation state' }} />
-    ))
-    .add('Validation State | Information', () => (
         <SearchInput
-            placeholder='Default'
-            searchList={searchData}
-            validationState={{ state: 'information', text: 'Test validation state' }} />
-    ))
-    .add('Validation State | Success', () => (
-        <SearchInput
-            placeholder='Default'
+            placeholder='Success'
             searchList={searchData}
             validationState={{ state: 'success', text: 'Test validation state' }} />
-    ))
-    .add('disable styles', () => (
         <SearchInput
-            disableStyles
-            {...createProps()} />
-    ));
+            placeholder='Information'
+            searchList={searchData}
+            validationState={{ state: 'information', text: 'Test validation state' }} />
+    </>
+);
+
+export const dev = () => (
+    <SearchInput
+        compact={boolean('compact', false)}
+        placeholder={'Select a Fruit'}
+        searchList={searchData}
+        validationState={select('Validation State', {
+            'none': '',
+            'success': { state: 'success', text: 'placeholder text' },
+            'error': { state: 'error', text: 'placeholder text' },
+            'information': { state: 'information', text: 'placeholder text' },
+            'warning': { state: 'warning', text: 'placeholder text' }
+        })} />
+);
+dev.story = {
+    parameters: { docs: { disable: true } }
+};
