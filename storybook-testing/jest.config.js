c4121d4c87a58884bb3e7a66bff28d23b2304279
jbadan <jenna.badanowski@gmail.com>
2020-05-05 06:23:35 -0700

    move testing into one command

--- storybook-testing/jest.config.js
+++ storybook-testing/jest.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    'rootDir': '../visual-regression-testing/',
+    'rootDir': '../storybook-testing/',
     'verbose': true,
     'preset': 'jest-puppeteer',
     'setupFilesAfterEnv': ['../config/jest/setup.js'],
