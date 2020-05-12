788814e2ec2c774366190725519abea0e053817f
jbadan <jenna.badanowski@gmail.com>
2020-05-12 15:17:24 -0700

    move containers inside

--- .storybook/custom/components/DocsPage.js
+++ .storybook/custom/components/DocsPage.js
@@ -25,7 +25,8 @@ const DocsPage = () => {
                 headingSelector: 'h2.sbdocs-h2, h3.sbdocs-h3, h4.sbdocs-h4',
                 orderedList: true,
                 scrollContainer: '.sbdocs-wrapper',
-                collapseDepth: 3
+                collapseDepth: 3,
+                hasInnerContainers: true
             }
         );
         document.querySelectorAll('.toc-link').forEach( x => x.setAttribute('target','_self'));
@@ -44,9 +45,9 @@ const DocsPage = () => {
             <Stories />
             <h2 className='sbdocs-h2' id='properties'>Properties</h2>
             <Props />
+            <Community />
+            <Footer />
         </div>
-        <Community />
-        <Footer />
       </>
     )
 }
