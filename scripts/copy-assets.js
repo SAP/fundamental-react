99a8d94aa04485f7b637522b5994f4ce5d58a709
jbadan <jenna.badanowski@gmail.com>
2020-05-12 13:45:37 -0700

    add header and footer to docs

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -28,16 +28,29 @@ if (!fs.existsSync('src/Docs')) {
     fs.mkdirSync('src/Docs');
 }
 
-// copy readme file and prepend necessary <Meta /> tag for storybook
+// copy readme file and prepend necessary <Meta />, <Header />, <Community /> and <Footer />
 fs.copyFile('README.md', introductionPath, (err) => {
     if (err) throw err;
 
     const data = fs.readFileSync(introductionPath).toString().split('\n');
-    data.splice(0, 0, 'import { Meta } from \'@storybook/addon-docs/blocks\';\n\n <Meta title=\'Introduction/Overview\' />\n' );
+    data.splice(0, 0, `
+import Community from \'../../.storybook/custom/components/Community\';
+import Header from \'../../.storybook/custom/components/Header\';
+import Footer from \'../../.storybook/custom/components/Footer\';
+import { Meta } from \'@storybook/addon-docs/blocks\';
+<Header />\n
+<Meta title=\'Introduction/Overview\' />\n` );
     const text = data.join('\n');
 
     fs.writeFile(introductionPath, text, function(writeErr) {
         if (writeErr) return writeErr;
+    });
+
+    fs.appendFile(introductionPath, `
+<Community />
+<Footer />
+`, function(isError) {
+        if (isError) throw isError;
     });
 
 });
