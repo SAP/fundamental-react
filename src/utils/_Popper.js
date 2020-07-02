5cfded7142409a9377d5ea097ef439bec6ed0153
Sam Kvale <sam.kvale@sap.com>
2020-07-02 13:41:15 -0500

    fix(popper): use string or undefined for x-out-of-bounds attribute

--- src/utils/_Popper.js
+++ src/utils/_Popper.js
@@ -184,7 +184,8 @@ class Popper extends React.Component {
                             className={popperClasses}
                             ref={ref}
                             style={{ ...style, ...popperProps.style, ...fundamentalStyleOverrides }}
-                            x-out-of-boundaries={isReferenceHidden}
+                            // eslint-disable-next-line no-undefined
+                            x-out-of-boundaries={isReferenceHidden ? 'true' : undefined}
                             // This is needed for fundamental-styles even though popper-2 uses data-placement as well
                             x-placement={placement}>
                             <div ref={innerRef}>
