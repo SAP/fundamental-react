080988d06f5c4fd98074f7fb7def2b4ff1966c7e
Mike <mike.diaz.covarrubias@sap.com>
2020-07-23 17:17:47 -0700

    fix: add allowFocusOnDisabled capability to button

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,6 +1,7 @@
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
+import shortId from '../utils/shortId';
 import { BUTTON_OPTIONS, BUTTON_TYPES } from '../utils/constants';
 import 'fundamental-styles/dist/icon.css';
 import 'fundamental-styles/dist/button.css';
@@ -40,19 +41,86 @@ const Button = React.forwardRef(({
         className
     );
 
-    return (<button {...props} className={buttonClasses}
-        disabled={disabled} onClick={onClick}
-        ref={ref}
-        selected={selected}
-        type={typeAttr}>
-        {children}
-    </button>);
+    const ariaDisabled = props['aria-disabled'];
+    let disabledInAnyWay = disabled || ariaDisabled;
+
+    //if button is disabled and focusable, remove onClick handler
+    let onClickHandler = allowFocusOnDisable && disabledInAnyWay ? () => {} : onClick;
+
+    const ariaLiveId = shortId.generate();
+
+    const getDisabledProps = () => {
+        let disabledProps = {};
+        if (allowFocusOnDisable) {
+            disabledProps['aria-disabled'] = disabledInAnyWay;
+            disabledProps['aria-describedby'] = ariaLiveId;
+        } else {
+            disabledProps.disabled = disabledInAnyWay;
+        }
+        return disabledProps;
+    };
+
+    const liveMessageForScreenReaders = disabledInAnyWay ? disabledMessage : enabledMessage;
+
+    const renderButtonStateMessage = () => {
+        let content = '';
+        if (allowFocusOnDisable && disabledInAnyWay !== null) {
+            content = (
+                <p
+                    aria-live='assertive'
+                    className={'fd-button__instructions'}
+                    id={ariaLiveId}>
+                    {liveMessageForScreenReaders}
+                </p>)
+            ;
+        }
+        return content;
+    };
+    return (
+        <>
+            <button
+                {...props}
+                {...getDisabledProps()}
+                className={buttonClasses}
+                onClick={onClickHandler}
+                ref={ref}
+                selected={selected}
+                type={typeAttr}>
+                {children}
+            </button>
+            {renderButtonStateMessage()}
+        </>
+    );
 });
 
 Button.displayName = 'Button';
 
+const validateStateTransitionMessage = (props, propName) => {
+    if (props.allowFocusOnDisable && (props.disabled !== null || props['aria-disabled']) && !props[propName]) {
+        return new Error(`
+        Missing or invalid value for ${propName}.
+        If this button can recieve focus when disabled (i.e. allowFocusOnDisable prop is true),
+        you need to provide localized string values for the enabled and disabled states.
+        These strings are used to annouce the state change by assitive techonolgies.
+        Hence, these should be specified for a better, accessible experience.
+        For e.g. 
+        <Button
+            disabled={someLogic()}
+            allowFocusOnDisable
+            disabledMessage="Please complete required form fields to enable form submission."
+            enabledMessage="Please press the submit button to proceed.">
+            Submit
+        </Button>
+        `);
+    }
+};
+
 Button.propTypes = {
+    /** Set to **true** to enable button focus through keyboard on a disabled button */
     allowFocusOnDisable: PropTypes.bool,
+    /** Automatically set when disabled prop is passed into button */
+    'aria-disabled': PropTypes.bool,
+    ariaDisabled: PropTypes.bool,
     /** Node(s) to render within the component */
     children: PropTypes.node,
     /** CSS class(es) to add to the element */
@@ -61,8 +129,10 @@ Button.propTypes = {
     compact: PropTypes.bool,
     /** Set to **true** to mark component as disabled and make it non-interactive */
     disabled: PropTypes.bool,
-    disabledMessage: PropTypes.string,
-    enabledMessage: PropTypes.string,
+    /** Message for screen reader to inform user the button is disabled */
+    disabledMessage: validateStateTransitionMessage,
+    /** Message for screen reader to inform user the button is enabled */
+    enabledMessage: validateStateTransitionMessage,
     /** The icon to include. See the icon page for the list of icons */
     glyph: PropTypes.string,
     /** Indicates the importance of the button: 'empahsized' or 'transparent' */
