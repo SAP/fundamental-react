6dd9a39180c13c24505078d7f6a6f831adf4eac9
Prasad Thakur <prasad.thakur@sap.com>
2020-08-13 15:24:54 -0700

    use defaultId if available

--- src/utils/useUniqueId.js
+++ src/utils/useUniqueId.js
@@ -1,7 +1,8 @@
 import shortid from './shortId';
 import { useState } from 'react';
 
-export default () => {
+export default (defaultId) => {
+    if (defaultId) return defaultId;
     const [uniqueId, setUniqueId] = useState();
     if (uniqueId) return uniqueId;
     const id = shortid.generate();
