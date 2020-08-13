a0dc3fcc8ab71e301046448cc510a0252a9b0224
Prasad Thakur <prasad.thakur@sap.com>
2020-08-13 15:25:02 -0700

    update stories

--- src/List/__stories__/List.stories.js
+++ src/List/__stories__/List.stories.js
@@ -5,11 +5,11 @@ import ListHeader from '../_ListHeader';
 import ListIcon from '../_ListIcon';
 import ListItem from '../_ListItem';
 import ListText from '../_ListText';
-import React from 'react';
 import {
     boolean,
     text
 } from '@storybook/addon-knobs';
+import React, { useState } from 'react';
 
 export default {
     title: 'Component API/List',
@@ -23,16 +23,16 @@ export default {
 export const simple = () => (
     <List>
         <List.Item>
-            <List.Text>List Item 1</List.Text>
-        </List.Item>
-        <List.Item selected>
-            <List.Text>List Item 2</List.Text>
+            <List.Text>Apple</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 3</List.Text>
+            <List.Text>Banana</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Durian</List.Text>
+        </List.Item>
+        <List.Item>
+            <List.Text>Grape</List.Text>
         </List.Item>
     </List>
 );
@@ -41,20 +41,20 @@ simple.storyName = 'Simple List';
 
 export const headerFooter = () => (
     <List>
-        <List.Header>List Header</List.Header>
+        <List.Header>Fruits</List.Header>
         <List.Item>
-            <List.Text>List Item 1</List.Text>
+            <List.Text>Apple</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 2</List.Text>
+            <List.Text>Banana</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 3</List.Text>
+            <List.Text>Durian</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Grape</List.Text>
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
+        <List.Footer>Updated 3 minutes ago</List.Footer>
     </List>
 );
 
@@ -62,109 +62,110 @@ headerFooter.storyName = 'with Header and Footer';
 
 export const noBorder = () => (
     <List noBorder>
-        <List.Header>List Header</List.Header>
+        <List.Header>Fruits</List.Header>
         <List.Item>
-            <List.Text>List Item 1</List.Text>
+            <List.Text>Apple</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 2</List.Text>
+            <List.Text>Banana</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 3</List.Text>
+            <List.Text>Durian</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Grape</List.Text>
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
+        <List.Footer>Updated 3 minutes ago</List.Footer>
     </List>
 );
 
 noBorder.storyName = 'with no borders';
 
 export const compact = () => (
-    <List compact>
-        <List.Header>List Header</List.Header>
+    <List compact >
+        <List.Header>Fruits</List.Header>
         <List.Item>
-            <List.Text>List Item 1</List.Text>
+            <List.Text>Apple</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 2</List.Text>
+            <List.Text>Banana</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 3</List.Text>
+            <List.Text>Durian</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Grape</List.Text>
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
+        <List.Footer>Updated 3 minutes ago</List.Footer>
     </List>
 );
 
 export const iconsLeft = () => (
-    <List>
-        <List.Header>List Header</List.Header>
+    <List >
+        <List.Header>Settings</List.Header>
         <List.Item>
             <List.Icon glyph='cart' />
-            <List.Text>List Item 1</List.Text>
+            <List.Text>Checkout Cart</List.Text>
         </List.Item>
         <List.Item>
             <List.Icon glyph='wrench' />
-            <List.Text>List Item 2</List.Text>
+            <List.Text>Profile Settings</List.Text>
         </List.Item>
         <List.Item>
             <List.Icon glyph='lightbulb' />
-            <List.Text>List Item 3</List.Text>
+            <List.Text>Information</List.Text>
         </List.Item>
         <List.Item>
             <List.Icon glyph='history' />
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Order History</List.Text>
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
     </List>
 );
 export const iconsRight = () => (
-    <List>
-        <List.Header>List Header</List.Header>
+    <List >
+        <List.Header>Settings</List.Header>
         <List.Item>
-            <List.Text>List Item 1</List.Text>
+            <List.Text>Checkout Cart</List.Text>
             <List.Icon glyph='navigation-right-arrow' />
         </List.Item>
         <List.Item>
-            <List.Text>List Item 2</List.Text>
+            <List.Text>Profile Settings</List.Text>
             <List.Icon glyph='navigation-right-arrow' />
         </List.Item>
         <List.Item>
-            <List.Text>List Item 3</List.Text>
+            <List.Text>Information</List.Text>
             <List.Icon glyph='navigation-right-arrow' />
         </List.Item>
         <List.Item>
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Order History</List.Text>
             <List.Icon glyph='navigation-right-arrow' />
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
     </List>
 );
 
 export const secondaryText = () => (
-    <List>
-        <List.Header>List Header</List.Header>
+    <List >
+        <List.Header>Order Receipt (4 items)</List.Header>
         <List.Item>
-            <List.Text>List Item 1</List.Text>
-            <List.Text secondary>Positive</List.Text>
+            <List.Text>Apple</List.Text>
+            <List.Text secondary>Quantity: 2</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 2</List.Text>
-            <List.Text secondary>Negative</List.Text>
+            <List.Text>Banana</List.Text>
+            <List.Text secondary>Quantity: 3</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 3</List.Text>
-            <List.Text secondary>Positive</List.Text>
+            <List.Text>Durian</List.Text>
+            <List.Text secondary>Quantity: 1</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 4</List.Text>
-            <List.Text secondary>Negative</List.Text>
+            <List.Text>Grape</List.Text>
+            <List.Text secondary>Quantity: 1</List.Text>
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
+        <List.Footer>
+            <List.Text><b>Total</b></List.Text>
+            <List.Text secondary>7</List.Text>
+        </List.Footer>
     </List>
 );
 
@@ -172,7 +173,7 @@ export const dev = () => (
     <List
         compact={boolean('compact', false)}
         noBorder={boolean('noBorder', false)}>
-        <List.Header>List Header</List.Header>
+        <List.Header>Developer's List</List.Header>
         <List.Item selected={boolean('selected', false)}>
             <List.Icon glyph={text('icon', 'accept')} />
             <List.Text
@@ -184,112 +185,137 @@ export const dev = () => (
 );
 
 export const byline = () => (
-    <List hasByline>
-        <List.Header>List Header</List.Header>
+    <List hasByline >
+        <List.Header>Order Summary (4 items)</List.Header>
         <List.Item>
-            <List.Text>List Item 1</List.Text>
-            <List.Byline>Byline</List.Byline>
+            <List.Text>Apple</List.Text>
+            <List.Byline>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</List.Byline>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 2</List.Text>
-            <List.Byline>Byline</List.Byline>
+            <List.Text>Banana</List.Text>
+            <List.Byline>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</List.Byline>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 3</List.Text>
-            <List.Byline>Byline</List.Byline>
+            <List.Text>Durian</List.Text>
+            <List.Byline>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</List.Byline>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Grape</List.Text>
             <List.Byline twoColumns>
-                <List.Text left>Left byline</List.Text>
-                <List.Text right>Right byline</List.Text>
+                <List.Text left>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</List.Text>
+                <List.Text right> Out of stock</List.Text>
             </List.Byline>
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
+        <List.Footer>Updated 3 minutes ago</List.Footer>
     </List>
 );
 
-export const selection = () => (
-    <List>
-        <List.Header>List Header</List.Header>
-        <List.Item>
-            <List.Selection>
-                <List.Text>List Item 1</List.Text>
-            </List.Selection>
-        </List.Item>
-        <List.Item>
-            <List.Selection>
-                <List.Text>List Item 2</List.Text>
-            </List.Selection>
-        </List.Item>
-        <List.Item>
-            <List.Selection>
-                <List.Text>List Item 3</List.Text>
-            </List.Selection>
-        </List.Item>
-        <List.Item>
-            <List.Selection>
-                <List.Text>List Item 4</List.Text>
-            </List.Selection>
-        </List.Item>
-        <List.Footer>List Footer</List.Footer>
-    </List>
-);
+export const selection = () => {
+    const [secondSelected, setSecondSelected] = useState(false);
+    const [thirdSelected, setThirdSelected] = useState(false);
+    return (
+        <>
+            <List
+                partialNavigation
+                selectable>
+                <List.Header>Checkout options</List.Header>
+                <List.Item selected>
+                    <List.Selection checkBoxAriaLabel='Coupon Applied: FREE'>
+                        <List.Text>Coupon Applied: FREE</List.Text>
+                    </List.Selection>
+                </List.Item>
+                <List.Item selected={secondSelected}>
+                    <List.Selection
+                        checkBoxAriaLabel='Add gift wrapping'
+                        onChange={(event) => setSecondSelected(event?.target?.checked)}
+                        selected>
+                        <List.Text>Add gift wrapping</List.Text>
+                    </List.Selection>
+                </List.Item>
+                <List.Item selected={thirdSelected}>
+                    <List.Selection
+                        checkBoxAriaLabel='Less packaging'
+                        onChange={(event) => setThirdSelected(event?.target?.checked)}
+                        selected>
+                        <List.Text>Less packaging</List.Text>
+                    </List.Selection>
+                </List.Item>
+                <List.Footer>Company ©</List.Footer>
+            </List>
+        </>
+    );
+};
 
 export const partialNavigation = () => (
     <List partialNavigation>
-        <List.Header>List Header</List.Header>
+        <List.Header>Cart (3 items)</List.Header>
         <List.Item url={'#'}>
-            <List.Text>List Item 1</List.Text>
+            <List.Text>Add more items</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 2</List.Text>
+            <List.Text>Apple</List.Text>
         </List.Item>
         <List.Item>
-            <List.Text>List Item 3</List.Text>
+            <List.Text>Banana</List.Text>
+        </List.Item>
+        <List.Item>
+            <List.Text>Durian</List.Text>
         </List.Item>
         <List.Item selected url={'#'}>
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Checkout</List.Text>
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
+        <List.Footer>Updated 3 minutes ago</List.Footer>
     </List>
 );
 
 export const navigationList = () => (
     <List navigation>
-        <List.Header>List Header</List.Header>
+        <List.Header>Quick links</List.Header>
         <List.Item url={'#'}>
-            <List.Text>List Item 1</List.Text>
+            <List.Text>Checkout Cart</List.Text>
         </List.Item>
         <List.Item url={'#'}>
-            <List.Text>List Item 2</List.Text>
+            <List.Text>Profile Settings</List.Text>
         </List.Item>
         <List.Item url={'#'}>
-            <List.Text>List Item 3</List.Text>
+            <List.Text>Information</List.Text>
         </List.Item>
         <List.Item selected url={'#'}>
-            <List.Text>List Item 4</List.Text>
+            <List.Text>Order History</List.Text>
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
+        <List.Footer>Company © </List.Footer>
     </List>
 );
 
 export const actionList = () => (
-    <List>
-        <List.Header>List Header</List.Header>
+    <List >
+        <List.Header>Cart (4 items)</List.Header>
         <List.Item>
-            <List.Text>List Item 1</List.Text>
+            <List.Text>Apple</List.Text>
+            <List.Text secondary>Quantity: 2</List.Text>
+        </List.Item>
+        <List.Item>
+            <List.Text>Banana</List.Text>
+            <List.Text secondary>Quantity: 3</List.Text>
+        </List.Item>
+        <List.Item>
+            <List.Text>Durian</List.Text>
+            <List.Text secondary>Quantity: 1</List.Text>
+        </List.Item>
+        <List.Item>
+            <List.Text>Grape</List.Text>
+            <List.Text secondary>Quantity: 1</List.Text>
         </List.Item>
         <List.Item action>
-            List Item 2
+            Empty cart
         </List.Item>
         <List.Item action>
-            List Item 3
+            Apply Coupon
         </List.Item>
         <List.Item action>
-            List Item 4
+            Checkout
         </List.Item>
-        <List.Footer>List Footer</List.Footer>
+        <List.Footer>Company ©</List.Footer>
     </List>
 );
 
