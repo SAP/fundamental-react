a11f636768007444b09c883f2c99859bc4e18fa1
Prasad Thakur <prasad.thakur@sap.com>
2020-08-14 14:06:40 -0700

    update test and storyshot

--- src/List/ListSelection.test.js
+++ src/List/ListSelection.test.js
@@ -5,7 +5,7 @@ import React from 'react';
 describe('<ListSelection />', () => {
     describe('Prop spreading', () => {
         test('should allow props to be spread to the ListSelection component', () => {
-            const element = mount(<List.Selection data-sample='Sample' />);
+            const element = mount(<List.Selection checkBoxAriaLabel='Select' data-sample='Sample' />);
 
             expect(
                 element.getDOMNode().attributes['data-sample'].value
