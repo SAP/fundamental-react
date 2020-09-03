0ee7d96225cd350d5073549421d858730d8163c2
Prasad Thakur <prasad.thakur@sap.com>
2020-09-03 09:23:35 -0700

    prop name change

--- src/Forms/_FormMessage.js
+++ src/Forms/_FormMessage.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import 'fundamental-styles/dist/form-message.css';
 
-const FormMessage = React.forwardRef(({ children, className, inPopoverList, type, ...props }, ref) => {
+const FormMessage = React.forwardRef(({ children, className, forPopoverList, type, ...props }, ref) => {
 
     const formMessageClasses = classnames(
         {
-            'fd-form-message': !inPopoverList,
-            'fd-list__message': inPopoverList,
-            [`fd-form-message--${type}`]: !inPopoverList && !!type,
-            [`fd-list__message--${type}`]: inPopoverList && !!type
+            'fd-form-message': !forPopoverList,
+            'fd-list__message': forPopoverList,
+            [`fd-form-message--${type}`]: !forPopoverList && !!type,
+            [`fd-list__message--${type}`]: forPopoverList && !!type
         },
         className
     );
@@ -34,8 +34,8 @@ FormMessage.propTypes = {
     children: PropTypes.node,
     /** CSS class(es) to add to the element */
     className: PropTypes.string,
-    /** Set to **true** if FormMessage is inside a popover*/
-    inPopoverList: PropTypes.bool,
+    /** Set to **true** if FormMessage is for a popover with list*/
+    forPopoverList: PropTypes.bool,
     /** Sets the variation of the component. Primarily used for styling */
     type: PropTypes.oneOf(FORM_MESSAGE_TYPES)
 };
