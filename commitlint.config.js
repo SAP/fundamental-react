7e41079fdbd6a49501f8544a4543e37d049b9041
deno <mladen.droshev@sap.com>
2022-04-07 09:42:04 -0400

    chore(ci): update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    'extends': ['@commitlint/config-conventional']
+    'extends': ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [
+            ['code', 'docs', 'e2e', 'release', 'deps', 'deps-dev', 'changelog', 'ci']
+        ],
+        'body-max-line-length': [2, 'always', 200],
+        'footer-max-line-length': [2, 'always', 200],
+        'header-max-length': [2, 'always', 200]
+    }
 };
