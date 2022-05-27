4f55385f0d826eec1df672429792d7079b02ce76
wchrapka <wojciech.chrapka@sap.com>
2022-05-27 17:50:52 +0200

    chore: babel config

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,8 @@ const defaultPresets = [
 
 const defaultPlugins = [
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     ['@babel/plugin-transform-runtime', { corejs: 3, proposals: true }]
 ];
 
