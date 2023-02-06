d958673ca0fe36f053d0f04baec3da5be8f4118c
InfiniteXyy <xuyiyangwing@gmail.com>
2023-02-06 15:28:38 +0800

    feat: add ts definition

---
+++ index.d.ts
@@ -0,0 +1,64 @@
+// Type definitions for fundamental-react 0.14
+// Project: https://sap.github.io/fundamental-react
+// Definitions by: Frank Feinbube <https://github.com/Feinbube>
+//                 Milan Proell <https://github.com/milanpro>
+//                 Auryn Engel <https://github.com/auryn31>
+//                 Shawn Xu <https://github.com/infinitexyy>
+
+export { default as ActionBar } from './types/ActionBar/ActionBar';
+export { default as Avatar } from './types/Avatar/Avatar';
+export { default as Breadcrumb } from './types/Breadcrumb/Breadcrumb';
+export { default as BusyIndicator } from './types/BusyIndicator/BusyIndicator';
+export { default as Button } from './types/Button/Button';
+export { default as ButtonGroup } from './types/Button/ButtonGroup';
+export { default as Calendar } from './types/Calendar/Calendar';
+export { default as ComboboxInput } from './types/ComboboxInput/ComboboxInput';
+export { default as Counter } from './types/Counter/Counter';
+export { default as DatePicker } from './types/DatePicker/DatePicker';
+export { default as Dialog } from './types/Dialog/Dialog';
+export { default as Checkbox } from './types/Forms/Checkbox';
+export { default as FormFieldset } from './types/Forms/FormFieldset';
+export { default as FormGroup } from './types/Forms/FormGroup';
+export { default as FormInput } from './types/Forms/FormInput';
+export { default as FormItem } from './types/Forms/FormItem';
+export { default as FormLabel } from './types/Forms/FormLabel';
+export { default as FormLegend } from './types/Forms/FormLegend';
+export { default as FormRadioGroup } from './types/Forms/FormRadioGroup';
+export { default as FormRadioItem } from './types/Forms/FormRadioItem';
+export { default as FormSelect } from './types/Forms/FormSelect';
+export { default as FormSet } from './types/Forms/FormSet';
+export { default as FormTextarea } from './types/Forms/FormTextarea';
+export { default as Icon } from './types/Icon/Icon';
+export { default as Image } from './types/Image/Image';
+export { default as InfoLabel } from './types/InfoLabel/InfoLabel';
+export { default as InlineHelp } from './types/InlineHelp/InlineHelp';
+export { default as InputGroup } from './types/InputGroup/InputGroup';
+export { default as LayoutPanel } from './types/LayoutPanel/LayoutPanel';
+export { default as Link } from './types/Link/Link';
+export { default as List } from './types/List/List';
+export { default as LocalizationEditor } from './types/LocalizationEditor/LocalizationEditor';
+export { default as Menu } from './types/Menu/Menu';
+export { default as MessageStrip } from './types/MessageStrip/MessageStrip';
+export { default as MultiInput } from './types/MultiInput/MultiInput';
+export { default as ObjectStatus } from './types/ObjectStatus/ObjectStatus';
+export { default as Pagination } from './types/Pagination/Pagination';
+export { default as Popover } from './types/Popover/Popover';
+export { default as SearchInput } from './types/SearchInput/SearchInput';
+export { default as Select } from './types/Select/Select';
+export { default as Shellbar } from './types/Shellbar/Shellbar';
+export { default as SideNav } from './types/SideNavigation/SideNav';
+export { default as StepInput } from './types/StepInput/StepInput';
+export { default as Switch } from './types/Switch/Switch';
+export { default as Tab } from './types/Tabs/Tab';
+export { default as TabGroup } from './types/Tabs/TabGroup';
+export { default as Table } from './types/Table/Table';
+export { default as Tile } from './types/Tile/Tile';
+export { default as Time } from './types/Time/Time';
+export { default as TimePicker } from './types/TimePicker/TimePicker';
+export { default as Title } from './types/Title/Title';
+export { default as Token } from './types/Token/Token';
+export { default as Container } from './types/Container/Container';
+export { default as Row } from './types/Row/Row';
+export { default as Column } from './types/Column/Column';
+export { default as Tree } from './types/Tree/Tree';
+export { default as Wizard } from './types/Wizard/Wizard';
