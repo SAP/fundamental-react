6583bd48e9f47b353f2707e93942a85861591315
Vita Chen <vitachen1228@gmail.com>
2023-02-06 22:56:56 +0800

    fix: popper

--- src/utils/_Popper.js
+++ src/utils/_Popper.js
@@ -13,6 +13,101 @@ import popoverStyles from 'fundamental-styles/dist/popover.css';
 
 const classnames = classnamesBind.bind(popoverStyles);
 
+// eslint-disable-next-line no-undefined
+const _get = (obj, path, defaultValue = undefined) => {
+    const travel = regexp =>
+        String.prototype.split
+            .call(path, regexp)
+            .filter(Boolean)
+            // eslint-disable-next-line no-undefined
+            .reduce((res, key) => (res !== null && res !== undefined ? res[key] : res), obj);
+    const result = travel(/[,[\]]+?/) || travel(/[,[\].]+?/);
+    // eslint-disable-next-line no-undefined
+    return result === undefined || result === obj ? defaultValue : result;
+};
+const poperMappings = {
+    'top-start': {
+        body: 'above',
+        arrow: 'bottom'
+    },
+    'top': {
+        body: 'above',
+        bodyX: 'center',
+        arrow: 'bottom',
+        arrowX: 'center'
+    },
+    'top-end': {
+        body: 'above',
+        bodyX: 'end',
+        arrow: 'bottom',
+        arrowX: 'end'
+    },
+    'left-start': {
+        body: 'before',
+        arrow: 'right'
+    },
+    'left': {
+        body: 'before',
+        bodyX: 'middle',
+        arrow: 'right',
+        arrowY: 'center'
+    },
+    'left-end': {
+        body: 'before',
+        bodyX: 'bottom',
+        arrow: 'right',
+        arrowY: 'end'
+    },
+    'bottom-start': {
+    },
+    'bottom': {
+        bodyX: 'center',
+        arrowX: 'center'
+    },
+    'bottom-end': {
+        bodyX: 'end',
+        arrowX: 'end'
+    },
+    'right-start': {
+        body: 'after',
+        arrow: 'left'
+    },
+    'right': {
+        body: 'after',
+        bodyX: 'middle',
+        arrow: 'left',
+        arrowY: 'center'
+    },
+    'right-end': {
+        body: 'after',
+        bodyX: 'bottom',
+        arrow: 'left',
+        arrowY: 'end'
+    }
+};
+const classPlaceMapping = {
+    body: '',
+    bodyX: '',
+    arrow: 'arrow-',
+    arrowX: 'arrow-x-',
+    arrowY: 'arrow-y-'
+};
+const getPopperClasses2 = (cssBlock, placement)=> {
+    const config = _get(poperMappings, placement);
+    let popperClasses = [];
+    if (config) {
+        const keys = Object.keys(config);
+        popperClasses = keys.map(key=> {
+            const relatedFix = _get(classPlaceMapping, key);
+            const value = _get(config, key);
+            return `${cssBlock}__body--${relatedFix}${value}`;
+        });
+    }
+
+    return popperClasses.join(' ');
+};
+
+
 const defaultModifiers = [
     {
         name: 'computeStyle',
@@ -26,8 +121,6 @@ const defaultModifiers = [
     },
     {
         name: 'hide' // adds the isReferenceHidden attribute
-
-
     }
 ];
 
@@ -181,9 +274,6 @@ class Popper extends React.Component {
         if (widthSizingType === 'maxTarget') {
             modifiers.push(maxTargetModifier);
         }
-        const popperClasses = classnames(`${cssBlock}__body`, popperClassName, {
-            [`${cssBlock}__body--no-arrow`]: !!noArrow
-        });
 
         const basePlacement = Array.isArray(popperPlacement)
             ? popperPlacement[0]
@@ -205,23 +295,28 @@ class Popper extends React.Component {
                         margin: 0
                     };
                     return (
-                        <div
-                            {...popperProps}
-                            className={popperClasses}
-                            ref={ref}
-                            style={{ ...style, ...popperProps.style, ...fundamentalStyleOverrides }}
-                            // eslint-disable-next-line no-undefined
-                            x-out-of-boundaries={isReferenceHidden ? 'true' : undefined}
-                            // This is needed for fundamental-styles even though popper-2 uses data-placement as well
-                            x-placement={placement}>
-                            <div className={classnames(`${cssBlock}__wrapper`, innerRefClassName)} ref={innerRef}>
-                                {children}
+                        <div className={cssBlock}>
+                            <div
+                                {...popperProps}
+                                className={classnames(`${cssBlock}__body`, popperClassName, {
+                                    [`${cssBlock}__body--no-arrow`]: !!noArrow
+                                }, getPopperClasses2(cssBlock, placement))}
+                                ref={ref}
+                                style={{ ...style, ...popperProps.style, ...fundamentalStyleOverrides }}
+                                // eslint-disable-next-line no-undefined
+                                x-out-of-boundaries={isReferenceHidden ? 'true' : undefined}
+                                // This is needed for fundamental-styles even though popper-2 uses data-placement as well
+                                x-placement={placement}>
+                                <div className={classnames(`${cssBlock}__wrapper`, innerRefClassName)} ref={innerRef}>
+                                    {children}
+                                </div>
+                                <span
+                                    className={classnames(`${cssBlock}__arrow`)}
+                                    ref={arrowProps.ref}
+                                    style={{ ...arrowProps.style, ...fundamentalStyleArrowOverrides }} />
                             </div>
-                            <span
-                                className={classnames(`${cssBlock}__arrow`)}
-                                ref={arrowProps.ref}
-                                style={{ ...arrowProps.style, ...fundamentalStyleArrowOverrides }} />
                         </div>
+
                     );
                 }}
             </ReactPopper>
